d2760adba692aac35744b86a75f828c28333ec69
Karim <k.guezlane@gmail.com>
2022-04-04 15:14:36 +0100

    updating User Model Schema

--- server/models/user.js
+++ server/models/user.js
@@ -4,7 +4,9 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
 	name: { type: String, required: true },
 	email: { type: String, required: true },
-	password: { type: String, required: true },
+	password: { type: String },
+	profilePic: { type: String },
+	frinds: [{ type: Schema.Types.ObjectId, ref: "User" }],
 });
 
 module.exports = mongoose.model("User", UserSchema);
