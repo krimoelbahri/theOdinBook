a9a8dff67ccec6c68cdfd5acf9dea3804e04f3e7
Karim <k.guezlane@gmail.com>
2022-04-05 12:35:26 +0100

    added Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/signup' element={<Signup />} />
+				<Route path='/signin' element={<Signin />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
