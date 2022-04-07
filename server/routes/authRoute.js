9e98c5b04852f0e68cefbe56e4f8623a9fe35d2e
Karim <k.guezlane@gmail.com>
2022-04-07 15:22:53 +0100

    facebook auth routes

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -7,10 +7,19 @@ var authController = require("../controllers/authController");
 //Signup
 //Link: /api/user/signup
 router.post("/signup", authController.signupUser);
-//Signin
 
 //local Signin
 //Link: /api/user/auth/local
 router.post("/auth/local", authController.localSigninUser);
+
+//Facebook Signin
+//Link: /api/user/auth/facebook
+router.get("/auth/facebook", authController.facebookSigninUser);
+
+//Facebook callback
+//Link: /api/user/auth/facebook/callback
+router.get("/auth/facebook/callback", authController.facebookSigninUserCB);
+//Logout
+router.get("/auth/logout", authController.logout);
 
 module.exports = router;
