31574549d4adb3b823180f365b608c39b8d7d7b2
Karim <k.guezlane@gmail.com>
2022-04-07 15:22:36 +0100

    cleaning user model

--- server/models/user.js
+++ server/models/user.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-	name: { type: String, required: true },
-	email: { type: String, required: true },
+	name: { type: String },
+	email: { type: String },
+	facebookId: { type: String },
 	password: { type: String },
 	profilePic: { type: String },
 	frinds: [{ type: Schema.Types.ObjectId, ref: "User" }],
 });
-
 module.exports = mongoose.model("User", UserSchema);
