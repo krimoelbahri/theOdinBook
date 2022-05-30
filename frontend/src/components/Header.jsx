5e34a681ea78c4a04478fd16df87f23111396ed6
Karim <k.guezlane@gmail.com>
2022-05-30 19:31:34 +0100

    cleaning up

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,22 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../features/auth/userSlice";
+import { Container } from "../styles/header.styled";
 
 function Header() {
 	const dispatch = useDispatch();
+	const { user } = useSelector((state) => state.user);
 	return (
-		<div>
-			<button
-				onClick={() => {
-					dispatch(logout());
-				}}
-			>
-				Logout
-			</button>
-		</div>
+		<Container>
+			{user && (
+				<button
+					onClick={() => {
+						dispatch(logout());
+					}}
+				>
+					Logout
+				</button>
+			)}
+		</Container>
 	);
 }
 
