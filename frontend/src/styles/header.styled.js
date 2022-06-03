d46eb64bdac9b4eeff7845eacd56b854ec8730e4
Karim <k.guezlane@gmail.com>
2022-06-03 15:42:04 +0100

    styling header

--- frontend/src/styles/header.styled.js
+++ frontend/src/styles/header.styled.js
@@ -6,5 +6,24 @@ export const Container = styled.div`
 	display: flex;
 	flex-direction: row;
 	align-items: center;
-	justify-content: space-evenly;
+	justify-content: space-around;
+	background-color: #2d9dd3;
+	h4 {
+		font-size: 30px;
+		font-style: italic;
+		font-weight: bold;
+		font-family: "klavika";
+		color: white;
+	}
+	.logo {
+		@media (max-width: 600px) {
+			display: none;
+		}
+	}
+	.logo-mobile {
+		display: none;
+		@media (max-width: 600px) {
+			display: block;
+		}
+	}
 `;
