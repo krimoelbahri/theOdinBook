033e15d039b2fbf09a28118d9116b8ffddebca3a
Karim <k.guezlane@gmail.com>
2022-06-05 15:25:26 +0100

    added create post component

--- frontend/src/components/home components/Main.jsx
+++ frontend/src/components/home components/Main.jsx
@@ -1,8 +1,23 @@
-import React from "react";
-import { MainContainer } from "../../styles/home.styled";
+import { useSelector } from "react-redux";
+import { MainContainer, CreatePostContainer } from "../../styles/home.styled";
 
 function Main() {
-	return <MainContainer>Main</MainContainer>;
+	const { user } = useSelector((state) => state.user);
+
+	return (
+		<MainContainer>
+			<CreatePostContainer>
+				<div className='subdiv up b-b'>
+					<img className='c-p' src='' alt='Pr' />
+					<p className='c-p'>What's on your mind, {user.name}?</p>
+				</div>
+				<div className='subdiv down c-p'>
+					<i className='fa-solid fa-photo-film'></i>
+					<p>Photo/Video</p>
+				</div>
+			</CreatePostContainer>
+		</MainContainer>
+	);
 }
 
 export default Main;
