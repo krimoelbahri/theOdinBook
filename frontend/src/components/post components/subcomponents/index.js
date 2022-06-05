62513bf83cd27bf62a27d39c209f77cfbc3fe582
Karim <k.guezlane@gmail.com>
2022-06-05 18:51:09 +0100

    exporting post subcomponents

---
+++ frontend/src/components/post components/subcomponents/index.js
@@ -0,0 +1,7 @@
+import PostComments from "./PostComments";
+import PostDescription from "./PostDescription";
+import PostHeader from "./PostHeader";
+import PostImg from "./PostImg";
+import PostReactions from "./PostReactions";
+
+export { PostHeader, PostDescription, PostImg, PostReactions, PostComments };
