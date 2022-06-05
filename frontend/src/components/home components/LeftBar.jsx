4e888a8174f0c7b87386033840617a3a42ba74e4
Karim <k.guezlane@gmail.com>
2022-06-05 15:24:51 +0100

    added  content to leftSidebar

--- frontend/src/components/home components/LeftBar.jsx
+++ frontend/src/components/home components/LeftBar.jsx
@@ -2,7 +2,28 @@ import React from "react";
 import { LBContainer } from "../../styles/home.styled";
 
 function LeftBar() {
-	return <LBContainer>LeftBar</LBContainer>;
+	return (
+		<LBContainer>
+			<ul>
+				<li>
+					<i className='fa-solid fa-newspaper'></i>
+					<p>Latest News</p>
+				</li>
+				<li>
+					<i className='fa-solid fa-user-group' />
+					<p>Friends</p>
+				</li>
+				<li>
+					<i className='fa-solid fa-users-line'></i>
+					<p>Groups</p>
+				</li>
+				<li>
+					<i className='fa-solid fa-tv'></i>
+					<p>Watch</p>
+				</li>
+			</ul>
+		</LBContainer>
+	);
 }
 
 export default LeftBar;
