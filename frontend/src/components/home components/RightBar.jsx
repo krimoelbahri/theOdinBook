2ac3cb3b2cc1136f7162a951ac274f63cdd8886c
Karim <k.guezlane@gmail.com>
2022-06-05 15:25:03 +0100

    added  content to rightSidebar

--- frontend/src/components/home components/RightBar.jsx
+++ frontend/src/components/home components/RightBar.jsx
@@ -2,7 +2,23 @@ import React from "react";
 import { RBContainer } from "../../styles/home.styled";
 
 function RightBar() {
-	return <RBContainer>RightBar</RBContainer>;
+	return (
+		<RBContainer>
+			<div className='subDiv contacts'>
+				<h5>Contacts</h5>
+				<i className='fa-solid fa-address-book'></i>
+			</div>
+			<div className='subDiv types'>
+				<div>
+					<h6>Primary</h6>
+				</div>
+				<div>
+					<h6>Suggested</h6>
+				</div>
+			</div>
+			<div>{/* TODO: adding profiles components*/}</div>
+		</RBContainer>
+	);
 }
 
 export default RightBar;
