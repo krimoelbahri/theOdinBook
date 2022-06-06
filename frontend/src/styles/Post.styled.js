6b14ff982cb08944364f4c5c76f2b50075feed5c
Karim <k.guezlane@gmail.com>
2022-06-06 20:02:39 +0100

    styling post components

--- frontend/src/styles/Post.styled.js
+++ frontend/src/styles/Post.styled.js
@@ -51,3 +51,65 @@ export const PostDescriptionContainer = styled.div`
 		padding: 5px;
 	}
 `;
+/* Post description */
+export const PostImgContainer = styled.div`
+	width: calc(100% + 40px);
+	margin: 5px 0 5px -20px;
+
+	img {
+		width: 100%;
+	}
+`;
+/* Post description */
+export const PostReactionContainer = styled.div`
+	width: 100%;
+	margin: 5px 0;
+	display: flex;
+	flex-direction: column;
+	p {
+		margin-left: 10px;
+		font-size: 14px;
+		color: gray;
+	}
+`;
+export const ReactionsStatsDiv = styled.div`
+	width: 100%;
+	height: 40px;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	div {
+		display: flex;
+		flex-direction: row;
+	}
+	p:hover {
+		text-decoration: underline;
+		cursor: pointer;
+	}
+`;
+export const ReactionButtonsDiv = styled.div`
+	width: 100%;
+	height: 40px;
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	div {
+		margin: 0 10px;
+		padding: 10px;
+		border-radius: 5px;
+		display: flex;
+		flex-direction: row;
+	}
+	div:hover {
+		background-color: rgb(1 1 1 / 5%);
+		cursor: pointer;
+	}
+	i {
+		color: rgb(1 1 1 / 50%);
+	}
+	p {
+		font-weight: bolder;
+	}
+`;
