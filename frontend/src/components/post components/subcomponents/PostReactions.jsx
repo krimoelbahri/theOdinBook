3c3ed924177805c884b29575fabeedfec5ff14aa
Karim <k.guezlane@gmail.com>
2022-06-06 20:02:26 +0100

    made component  for post reactions

--- frontend/src/components/post components/subcomponents/PostReactions.jsx
+++ frontend/src/components/post components/subcomponents/PostReactions.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import {
+	PostReactionContainer,
+	ReactionButtonsDiv,
+	ReactionsStatsDiv,
+} from "../../../styles/Post.styled";
 
 function PostReactions() {
-	return <div>PostReactions</div>;
+	return (
+		<PostReactionContainer>
+			<ReactionsStatsDiv className='b-b'>
+				<div>
+					<div>emojies</div>
+					<p>Likes Number</p>
+				</div>
+				<div>
+					<p>Comments Number</p>
+				</div>
+			</ReactionsStatsDiv>
+			<ReactionButtonsDiv>
+				<div>
+					<i className='fa-solid fa-thumbs-up'></i>
+					<p>Like</p>
+				</div>
+				<div>
+					<i className='fa-solid fa-message'></i>
+					<p>Comment</p>
+				</div>
+			</ReactionButtonsDiv>
+		</PostReactionContainer>
+	);
 }
 
 export default PostReactions;
