a2db0d0baa4c4690df8fdb2965155c7c344957ac
Karim <k.guezlane@gmail.com>
2022-06-07 19:27:26 +0100

    making post comment component

--- frontend/src/components/post components/subcomponents/PostComments.jsx
+++ frontend/src/components/post components/subcomponents/PostComments.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import {
+	PostCommentsContainer,
+	ShowCommentsDiv,
+	AddCommentsDiv,
+	CommentsDiv,
+} from "../../../styles/Post.styled";
 
 function PostComments() {
-	return <div>PostComments</div>;
+	function handleSubmit(e) {
+		e.preventDefault();
+	}
+	return (
+		<PostCommentsContainer>
+			<ShowCommentsDiv>
+				<p>show all</p>
+			</ShowCommentsDiv>
+			<AddCommentsDiv>
+				<img src='' alt='' />
+				<div>
+					<form onSubmit={handleSubmit}>
+						<input type='text' placeholder='Write a comment...' />
+						<button type='submit' className='c-p'>
+							<i className='fa-solid fa-paper-plane'></i>
+						</button>
+					</form>
+				</div>
+			</AddCommentsDiv>
+			<CommentsDiv>
+				<div>
+					<img src='' alt='' />
+					<div>
+						<div>
+							<p className='c-p'>profile name</p>
+							<span>this is a sample comment</span>
+						</div>
+					</div>
+					<i className='fa-solid fa-ellipsis c-p'></i>
+				</div>
+			</CommentsDiv>
+		</PostCommentsContainer>
+	);
 }
 
 export default PostComments;
