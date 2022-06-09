e69d6a8e1ffa5fe7415f70484a38eb90ddca5f41
Karim <k.guezlane@gmail.com>
2022-06-09 18:55:01 +0100

    styling Header

---
+++ frontend/src/styles/Header/header.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+	height: 60px;
+	width: 100%;
+	position: fixed;
+	top: 0;
+	z-index: 10;
+	background-color: #2d9dd3;
+`;
