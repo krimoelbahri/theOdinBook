35fd26c621f7c55682f26c0e32cf491139349da3
Karim <k.guezlane@gmail.com>
2022-06-09 18:55:21 +0100

    index file to export header styling

---
+++ frontend/src/styles/Header/index.js
@@ -0,0 +1,5 @@
+import { Container } from "./header.styled";
+import { LogoContainer } from "./logo.styled";
+import { SearchContainer } from "./SearchInput.styled";
+
+export { LogoContainer, Container, SearchContainer };
