51817eeed86c30f616c1dc70519be27f19444ce5
Karim <k.guezlane@gmail.com>
2022-06-09 18:54:33 +0100

    component to handle LOGO

---
+++ frontend/src/components/header components/subcomponents/Logo.jsx
@@ -0,0 +1,11 @@
+import { LogoContainer } from "../../../styles/Header";
+
+function Logo() {
+	return (
+		<LogoContainer>
+			<h4 className='logo'>BS</h4>
+		</LogoContainer>
+	);
+}
+
+export default Logo;
