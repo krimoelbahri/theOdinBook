92c7f31aa7bdea17fc7837c0a257736114e67eb5
Karim <k.guezlane@gmail.com>
2022-06-09 18:55:32 +0100

    logo styling

---
+++ frontend/src/styles/Header/logo.styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const LogoContainer = styled.div`
+	width: 40px;
+	height: 40px;
+	margin: 10px 10px;
+	border-radius: 50%;
+	background-color: white;
+	position: fixed;
+	left: 0;
+	h4 {
+		padding: 8px 7px;
+		font-size: 20px;
+		font-style: italic;
+		font-weight: bold;
+		font-family: "klavika";
+		color: #2d9dd3;
+	}
+`;
