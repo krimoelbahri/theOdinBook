8df06a0fb46273ac23b33f2fd187ed90089c4837
Karim <k.guezlane@gmail.com>
2022-06-10 18:26:34 +0100

    handling showing hiding DD

--- frontend/src/components/header components/subcomponents/SettingsAcount.jsx
+++ frontend/src/components/header components/subcomponents/SettingsAcount.jsx
@@ -1,56 +1,24 @@
+import { useState } from "react";
 import {
 	SettingsContainer,
 	ArrowContainer,
-	SettingsDropDown,
-	ProfileDiv,
-	SettingsDiv,
-	IconContainer,
+	SettingsDropDownContainer,
 } from "../../../styles/Header/SettingsAccount-styled";
+import SettingDropDown from "./SettingDropDown";
 
 function Setting() {
+	const [showDD, setShowDD] = useState(false); //DD = DropDown
+	function handleDropDown() {
+		setShowDD(!showDD);
+	}
 	return (
 		<SettingsContainer>
-			<ArrowContainer>
+			<ArrowContainer onClick={handleDropDown} active={showDD}>
 				<i className='fa-solid fa-caret-down'></i>
 			</ArrowContainer>
-			<SettingsDropDown>
-				<ProfileDiv>
-					<img className='c-p' src='' alt='' />
-					<div>
-						<p className='name '>Profile name</p>
-						<p className='date '>see your profile</p>
-					</div>
-				</ProfileDiv>
-				<hr />
-				<SettingsDiv>
-					<div className='hv fr-sb'>
-						<div className='fr-sb'>
-							<IconContainer>
-								<i className='fa-solid fa-gear'></i>
-							</IconContainer>
-							<p>Settings</p>
-						</div>
-						<i className='fa-solid fa-chevron-right'></i>
-					</div>
-					<div className='hv fr-sb'>
-						<div className='fr-sb'>
-							<IconContainer>
-								<i className='fa-solid fa-moon'></i>
-							</IconContainer>
-							<p>Display</p>
-						</div>
-						<i className='fa-solid fa-chevron-right'></i>
-					</div>
-					<div className='hv fr-sb'>
-						<div className='fr-sb'>
-							<IconContainer>
-								<i className='fa-solid fa-arrow-right-from-bracket'></i>
-							</IconContainer>
-							<p>Log Out</p>
-						</div>
-					</div>
-				</SettingsDiv>
-			</SettingsDropDown>
+			<SettingsDropDownContainer active={showDD}>
+				<SettingDropDown />
+			</SettingsDropDownContainer>
 		</SettingsContainer>
 	);
 }
