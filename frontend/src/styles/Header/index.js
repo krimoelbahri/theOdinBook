2f674c6baf46dfaf5cf347175e1e8159a2a11881
Karim <k.guezlane@gmail.com>
2022-06-10 15:13:28 +0100

    exporting header styling

--- frontend/src/styles/Header/index.js
+++ frontend/src/styles/Header/index.js
@@ -1,5 +1,23 @@
 import { Container } from "./header.styled";
 import { LogoContainer } from "./logo.styled";
 import { SearchContainer } from "./SearchInput.styled";
+import {
+	SettingsContainer,
+	ArrowContainer,
+	SettingsDropDown,
+	SettingsDiv,
+	IconContainer,
+} from "./SettingsAccount-styled";
+import { ShortcutsContainer } from "./shortcuts.styled";
 
-export { LogoContainer, Container, SearchContainer };
+export {
+	LogoContainer,
+	Container,
+	SearchContainer,
+	ShortcutsContainer,
+	SettingsContainer,
+	ArrowContainer,
+	SettingsDropDown,
+	SettingsDiv,
+	IconContainer,
+};
