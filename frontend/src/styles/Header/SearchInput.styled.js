ab4d2870445f00584ae73826468a5a9cb9f83423
Karim <k.guezlane@gmail.com>
2022-06-11 19:04:11 +0100

    styling search DD

--- frontend/src/styles/Header/SearchInput.styled.js
+++ frontend/src/styles/Header/SearchInput.styled.js
@@ -1,12 +1,36 @@
 import styled from "styled-components";
+export const SearchDDContainer = styled.div`
+	width: 320px;
+	border-radius: 0 10px 10px 10px;
+	background-color: white;
+	position: fixed;
+	left: 0px;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+`;
+export const DDheader = styled.div`
+	height: 60px;
+	//margin-left: 60px;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: space-evenly;
+	.arrow {
+		border-radius: 50%;
+		padding: 10px;
+		display: block;
+		:hover {
+			background-color: #efefef;
+			cursor: pointer;
+		}
+	}
+`;
 
 export const SearchContainer = styled.div`
-	width: 200px;
+	width: 240px;
 	height: 40px;
-	margin: 10px 10px;
-	position: fixed;
-	left: 60px;
-	padding: 2px 5px;
+	padding: 4px 10px;
 	background-color: #efefef;
 	border-radius: 20px;
 	display: flex;
@@ -22,13 +46,24 @@ export const SearchContainer = styled.div`
 	}
 	input {
 		background-color: #efefef;
-		width: 80%;
-		height: 100%;
+		width: 100%;
+		height: 90%;
 		border: none;
 		outline: none;
 	}
 	i {
 		margin: 0px 5px;
 		opacity: 0.4;
+		display: block;
+	}
+`;
+export const SearchDD = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	.recent {
+		height: 40px;
+		width: 100%;
+		padding: 5px 15px;
 	}
 `;
