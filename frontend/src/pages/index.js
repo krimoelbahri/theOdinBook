c7769c38ba62a3a47a19886fdc2869c11b628f2a
Karim <k.guezlane@gmail.com>
2022-06-15 11:54:33 +0100

    index to export pages

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+import Friends from "./Friends";
+import Groups from "./Groups";
+import Home from "./Home";
+import Signin from "./Signin";
+import Signup from "./Signup";
+import Watch from "./Watch";
+export { Groups, Home, Signin, Signup, Watch, Friends };
