80efaa6b2fcca91956c66edbc35bbb6d8d22fce9
Karim <k.guezlane@gmail.com>
2022-06-15 11:53:55 +0100

    imporing pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Signin from "./pages/Signin";
-import Signup from "./pages/Signup";
-import Home from "./pages/Home";
+import { Friends, Signin, Signup, Watch, Home } from "./pages";
 import Header from "./components/header components";
 import GlobalFonts from "./fonts/fonts";
 function App() {
@@ -13,6 +11,8 @@ function App() {
 				<Route path='/' element={<Home />} />
 				<Route path='/signup' element={<Signup />} />
 				<Route path='/signin' element={<Signin />} />
+				<Route path='/friends' element={<Friends />} />
+				<Route path='/watch' element={<Watch />} />
 			</Routes>
 			<GlobalFonts />
 		</>
