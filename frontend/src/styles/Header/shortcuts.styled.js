2947c8a8735772ce4bc9b5380ec8567fb6d36c98
Karim <k.guezlane@gmail.com>
2022-06-15 11:55:06 +0100

    styling shortcuts

--- frontend/src/styles/Header/shortcuts.styled.js
+++ frontend/src/styles/Header/shortcuts.styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+let boxShadow = "0px 0px 4px 1px rgba(209, 194, 194, 1)";
 
 export const ShortcutsContainer = styled.div`
 	width: 34%;
@@ -10,13 +11,21 @@ export const ShortcutsContainer = styled.div`
 	justify-content: space-between;
 	align-items: center;
 	@media (max-width: 700px) {
-		display: none;
+		width: 100%;
+		background-color: transparent;
+		box-shadow: ${boxShadow};
+		padding: 5px 30px;
+		bottom: 0;
+		margin: 0;
 	}
 `;
 export const ShortcutDiv = styled.div`
 	width: 22%;
 	height: 100%;
 	border-bottom: ${(props) => (props.active ? "4px solid white" : null)};
+	@media (max-width: 700px) {
+		border-bottom: ${(props) => (props.active ? "4px solid #2d9dd3" : null)};
+	}
 	.item-holder {
 		width: 100%;
 		height: 54px;
@@ -32,6 +41,9 @@ export const ShortcutDiv = styled.div`
 		i {
 			font-size: 23px;
 			color: ${(props) => (props.active ? "white" : "black")};
+			@media (max-width: 700px) {
+				color: ${(props) => (props.active ? "#2d9dd3" : "black")};
+			}
 		}
 	}
 `;
