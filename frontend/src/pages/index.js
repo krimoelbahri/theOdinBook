e8ffbfbd38d59c1e1ff854adf35e29bc4e46ec5d
Karim <k.guezlane@gmail.com>
2022-06-16 23:09:39 +0100

    exported header dashboard

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,7 +1,9 @@
 import Friends from "./Friends";
 import Groups from "./Groups";
+import HeaderDashboeard from "./HeaderDashboeard";
 import Home from "./Home";
 import Signin from "./Signin";
 import Signup from "./Signup";
 import Watch from "./Watch";
-export { Groups, Home, Signin, Signup, Watch, Friends };
+
+export { Groups, Home, Signin, Signup, Watch, Friends, HeaderDashboeard };
