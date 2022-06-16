cc01d86c69f4dca3f9dd93e897ee903ad1205aba
Karim <k.guezlane@gmail.com>
2022-06-16 23:08:31 +0100

    added links to leftbar elements

--- frontend/src/components/home components/LeftBar.jsx
+++ frontend/src/components/home components/LeftBar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { LBContainer } from "../../styles/home.styled";
 
 function LeftBar() {
@@ -6,20 +7,29 @@ function LeftBar() {
 		<LBContainer>
 			<ul>
 				<li>
-					<i className='fa-solid fa-newspaper'></i>
-					<p>Latest News</p>
+					<Link to={"/"}>
+						<i className='fa-solid fa-newspaper'></i>
+						<p>Latest News</p>
+					</Link>
 				</li>
 				<li>
-					<i className='fa-solid fa-user-group' />
-					<p>Friends</p>
+					<Link to={"/friends"}>
+						<i className='fa-solid fa-user-group' />
+						<p>Friends</p>
+					</Link>
 				</li>
 				<li>
-					<i className='fa-solid fa-users-line'></i>
-					<p>Groups</p>
+					<Link to={"/friends"}>
+						{/*TODO:change this to groups*/}
+						<i className='fa-solid fa-users-line'></i>
+						<p>Groups</p>
+					</Link>
 				</li>
 				<li>
-					<i className='fa-solid fa-tv'></i>
-					<p>Watch</p>
+					<Link to={"/watch"}>
+						<i className='fa-solid fa-tv'></i>
+						<p>Watch</p>
+					</Link>
 				</li>
 			</ul>
 		</LBContainer>
