465335fd5657560d8bb7a25b374796420878c671
Karim <k.guezlane@gmail.com>
2022-06-16 23:07:47 +0100

    added header dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Friends, Signin, Signup, Watch, Home } from "./pages";
-import Header from "./components/header components";
+import { Friends, Signin, Signup, Watch, Home, HeaderDashboeard } from "./pages";
 import GlobalFonts from "./fonts/fonts";
 function App() {
 	return (
 		<>
-			<Header />
 			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/signup' element={<Signup />} />
-				<Route path='/signin' element={<Signin />} />
-				<Route path='/friends' element={<Friends />} />
-				<Route path='/watch' element={<Watch />} />
+				<Route path='/' element={<HeaderDashboeard />}>
+					<Route path='' element={<Home />} />
+					<Route path='friends' element={<Friends />} />
+					<Route path='watch' element={<Watch />} />
+					<Route path='signup' element={<Signup />} />
+					<Route path='signin' element={<Signin />} />
+				</Route>
 			</Routes>
 			<GlobalFonts />
 		</>
