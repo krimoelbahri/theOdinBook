43f8c7047fd0cf9a676b2315afe8791b50a64e26
Karim <k.guezlane@gmail.com>
2022-06-18 22:03:44 +0100

    exporting profile styles

---
+++ frontend/src/styles/profile/index.js
@@ -0,0 +1,8 @@
+import {
+	TopSectionContainer,
+	CoverPicWrapper,
+	ProfileInfoWrapper,
+	ProfilePicWrapper,
+} from "./TopSection.styled";
+
+export { TopSectionContainer, CoverPicWrapper, ProfileInfoWrapper, ProfilePicWrapper };
