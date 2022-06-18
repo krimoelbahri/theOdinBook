37c39079c65a861519e72d557b5657c2185f0067
Karim <k.guezlane@gmail.com>
2022-06-18 22:02:27 +0100

    cover photo subcomponents

---
+++ frontend/src/components/profile components/subcomponents/CoverPic.jsx
@@ -0,0 +1,11 @@
+function CoverPic() {
+	return (
+		<div className='wrapper'>
+			<img src='*' alt='Cover' />
+			<button>Edit Cover Photo</button>
+			<div></div> {/*TODO: Edit DD */}
+		</div>
+	);
+}
+
+export default CoverPic;
