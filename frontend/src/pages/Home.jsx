ce12a4e9996d2ce5bdb50fc5c37afad02f3a00e2
Karim <k.guezlane@gmail.com>
2022-06-19 20:41:41 +0100

    cleaning

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,25 +1,12 @@
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
 import { LeftBar, RightBar, Main } from "../components/home components";
 import { Container } from "../styles/home.styled";
 
 const Home = () => {
-	const { user } = useSelector((state) => state.user);
-	const navigate = useNavigate();
-	useEffect(() => {
-		if (!user) navigate("/signin");
-	}, [user, navigate]);
-
 	return (
 		<Container>
-			{user && (
-				<>
-					<LeftBar />
-					<Main />
-					<RightBar />
-				</>
-			)}
+			<LeftBar />
+			<Main />
+			<RightBar />
 		</Container>
 	);
 };
