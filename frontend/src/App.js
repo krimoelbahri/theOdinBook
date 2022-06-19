763c591452144e97307811df4fdf4da41fb27603
Karim <k.guezlane@gmail.com>
2022-06-19 20:38:09 +0100

    seperated routes in their own component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,11 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { Friends, Signin, Signup, Watch, Home, HeaderDashboeard, Profile } from "./pages";
 import GlobalFonts from "./fonts/fonts";
+import MyRoutes from "./MyRoutes";
+
 function App() {
 	return (
 		<>
-			<Routes>
-				<Route path='/' element={<HeaderDashboeard />}>
-					<Route path='' element={<Home />} />
-					<Route path='friends' element={<Friends />} />
-					<Route path='watch' element={<Watch />} />
-					<Route path='signup' element={<Signup />} />
-					<Route path='signin' element={<Signin />} />
-					<Route path=':id' element={<Profile />} />
-				</Route>
-			</Routes>
+			<MyRoutes />
 			<GlobalFonts />
 		</>
 	);
