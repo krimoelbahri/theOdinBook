bc5f61dc5441cbeb17bc5ef579316ad5e163eac4
Karim <k.guezlane@gmail.com>
2022-06-19 20:41:53 +0100

    cleaning

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,10 +1,9 @@
 import Friends from "./Friends";
 import Groups from "./Groups";
-import HeaderDashboeard from "./HeaderDashboeard";
+import HeaderLayout from "./HeaderLayout";
 import Home from "./Home";
 import Profile from "./Profile";
 import Signin from "./Signin";
 import Signup from "./Signup";
 import Watch from "./Watch";
-
-export { Groups, Home, Signin, Signup, Watch, Friends, HeaderDashboeard, Profile };
+export { Groups, Home, Signin, Signup, Watch, Friends, HeaderLayout, Profile };
