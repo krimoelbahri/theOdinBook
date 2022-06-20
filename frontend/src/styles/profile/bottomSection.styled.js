f3b0710df05fdf5e42c8f4ac2e946f2184fc691c
Karim <k.guezlane@gmail.com>
2022-06-20 18:21:17 +0100

    styling bottom section

---
+++ frontend/src/styles/profile/bottomSection.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const BottomSectionContainer = styled.div`
+	width: 100%;
+	padding: 10px;
+
+	div {
+		width: 200px;
+		height: 400px;
+		background-color: white;
+	}
+`;
