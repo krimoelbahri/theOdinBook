9f1e5e30b3e3ee727c2648fbe41933b25f42b783
Karim <k.guezlane@gmail.com>
2022-06-20 18:21:04 +0100

    handleing visibility in nav section

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,9 +1,18 @@
-import { ProfileTopSection } from "../components/profile components";
+import {
+	ProfileTopSection,
+	ProfileNavSection,
+	ProfileBottomSection,
+} from "../components/profile components";
+import { useElementOnScreen } from "../hooks/intersectionObserver";
 
 function Profile() {
+	const [ref, isVisible] = useElementOnScreen({ rootMargin: "-60px" });
+
 	return (
 		<>
-			<ProfileTopSection />
+			<ProfileTopSection element={ref} />
+			<ProfileNavSection visible={isVisible} />
+			<ProfileBottomSection />
 		</>
 	);
 }
