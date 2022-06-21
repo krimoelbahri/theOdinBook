400d34ca805f7fda11a44f00694a5d0a0008e0a3
Karim <k.guezlane@gmail.com>
2022-06-21 22:18:38 +0100

    added theme styling properties

---
+++ frontend/src/styles/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+	//BGC back ground color
+	hoverBGC: "rgb(215 215 215 / 30%)",
+	headerBGC: "#2d9dd3",
+	commentsBGC: "#f0f2f5",
+	searchBGC: "#efefef",
+	iconBGC: "rgb(215 215 215)",
+	cardsBGC: "white",
+	//Box Shadows
+	mainBS: "0px 0px 4px 1px rgba(209, 194, 194, 1)",
+};
+export default theme;
