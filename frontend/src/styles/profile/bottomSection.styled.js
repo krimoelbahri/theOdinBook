093274e22a9be419d5438809bc6e3fadf3fd3d4c
Karim <k.guezlane@gmail.com>
2022-06-21 22:19:27 +0100

    styling using theme

--- frontend/src/styles/profile/bottomSection.styled.js
+++ frontend/src/styles/profile/bottomSection.styled.js
@@ -2,11 +2,26 @@ import styled from "styled-components";
 
 export const BottomSectionContainer = styled.div`
 	width: 100%;
-	padding: 10px;
-
-	div {
-		width: 200px;
-		height: 400px;
-		background-color: white;
+	padding-top: 20px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	.wrapper {
+		width: 100%;
+		max-width: 850px;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
 	}
+`;
+export const ProfileLeftBar = styled.div`
+	width: 30%;
+	position: sticky;
+	display: flex;
+	flex-direction: column;
+`;
+export const ProfilePostsWrapper = styled.div`
+	width: 60%;
+	display: flex;
+	flex-direction: column;
 `;
