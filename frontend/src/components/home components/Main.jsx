2b0e779dd375b9fe611a59842141f393a611738e
Karim <k.guezlane@gmail.com>
2022-06-21 22:16:07 +0100

    cleaning

--- frontend/src/components/home components/Main.jsx
+++ frontend/src/components/home components/Main.jsx
@@ -1,22 +1,12 @@
-import { useSelector } from "react-redux";
-import { MainContainer, CreatePostContainer } from "../../styles/home.styled";
-import Post from "../post components";
+import { MainContainer } from "../../styles/home.styled";
+import { Post, CreatePost } from "../post components";
 
 function Main() {
-	const { user } = useSelector((state) => state.user);
-
 	return (
 		<MainContainer>
-			<CreatePostContainer>
-				<div className='subdiv up b-b'>
-					<img className='c-p' src={user.profilePic} alt='Pr' />
-					{user && <p className='c-p'>What's on your mind, {user.name}?</p>}
-				</div>
-				<div className='subdiv down c-p'>
-					<i className='fa-solid fa-photo-film'></i>
-					<p>Photo/Video</p>
-				</div>
-			</CreatePostContainer>
+			<CreatePost />
+			<Post />
+			<Post />
 			<Post />
 		</MainContainer>
 	);
