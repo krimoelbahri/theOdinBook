18d7fe3ae1d61020e0f177d83a5c5ec07337e18a
Karim <k.guezlane@gmail.com>
2022-06-22 23:30:37 +0100

    added left bar component to profile

--- frontend/src/components/profile components/ProfileBottomSection.jsx
+++ frontend/src/components/profile components/ProfileBottomSection.jsx
@@ -1,10 +1,45 @@
-import { BottomSectionContainer, ProfileLeftBar, ProfilePostsWrapper } from "../../styles/profile";
+import { Link } from "react-router-dom";
+import {
+	BottomSectionContainer,
+	ProfileLeftBar,
+	ProfilePostsWrapper,
+	FriendsContainer,
+} from "../../styles/profile";
 import { Post, CreatePost } from "../post components";
+
 function ProfileBottomSection() {
 	return (
 		<BottomSectionContainer>
 			<div className='wrapper'>
-				<ProfileLeftBar></ProfileLeftBar>
+				<ProfileLeftBar>
+					<FriendsContainer>
+						<div className='header'>
+							<Link
+								to={`/62af5a0d2b41db9d923ba62d?sf=friends`}
+								state={{ data: "friends" }}
+							>
+								<h3>Friends</h3>
+							</Link>
+							<Link
+								to={`/62af5a0d2b41db9d923ba62d?sf=friends`}
+								state={{ data: "friends" }}
+							>
+								<h4>See all friends</h4>
+							</Link>
+						</div>
+						<div className='wrapper'>
+							{Array.from(Array(9)).map((_, i) => (
+								<div key={`card${i}`} className='card'>
+									<img
+										src='https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fA%3D%3D&w=1000&q=80'
+										alt='card'
+									/>
+									<h3>Profile Name</h3>
+								</div>
+							))}
+						</div>
+					</FriendsContainer>
+				</ProfileLeftBar>
 				<ProfilePostsWrapper>
 					<CreatePost />
 					<Post />
