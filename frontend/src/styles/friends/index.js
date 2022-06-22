9750aeb273eec5ef6bc28c7450ce4002d36074a2
Karim <k.guezlane@gmail.com>
2022-06-22 23:33:16 +0100

    exporting friends styles

---
+++ frontend/src/styles/friends/index.js
@@ -0,0 +1,9 @@
+import {
+	Container,
+	FriendsHeader,
+	FriendsCardsContainer,
+	CardContainer,
+	CardButton,
+} from "./friends.styled";
+
+export { Container, FriendsHeader, FriendsCardsContainer, CardContainer, CardButton };
