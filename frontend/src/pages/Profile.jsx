1a0dc7e731ea5fa4dd56d23963e23097d62b00f5
Karim <k.guezlane@gmail.com>
2022-06-22 23:32:17 +0100

    passing location to navSection

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,3 +1,4 @@
+import { useLocation } from "react-router-dom";
 import {
 	ProfileTopSection,
 	ProfileNavSection,
@@ -7,11 +8,11 @@ import { useElementOnScreen } from "../hooks/intersectionObserver";
 
 function Profile() {
 	const [ref, isVisible] = useElementOnScreen({ rootMargin: "-60px" });
-
+	const location = useLocation();
 	return (
 		<>
 			<ProfileTopSection element={ref} />
-			<ProfileNavSection visible={isVisible} />
+			<ProfileNavSection visible={isVisible} location={location} />
 			<ProfileBottomSection />
 		</>
 	);
