f059619eb27e67cdff3cfe12cb05642ed8cbc241
Karim <k.guezlane@gmail.com>
2022-06-23 23:13:45 +0100

    modal container BGC

--- frontend/src/styles/theme.js
+++ frontend/src/styles/theme.js
@@ -6,7 +6,8 @@ const theme = {
 	searchBGC: "#efefef",
 	iconBGC: "rgb(215 215 215)",
 	cardsBGC: "white",
+	modalContainerBGC: "rgba(244, 244, 244, 0.8);",
 	//Box Shadows
-	mainBS: "0px 0px 4px 1px rgba(209, 194, 194, 1)",
+	mainBS: "0px 0px 4px 1px rgb(194, 194, 194)",
 };
 export default theme;
