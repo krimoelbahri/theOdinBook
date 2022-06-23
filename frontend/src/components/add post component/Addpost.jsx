6b5cfdd7038f53d50aa14aa2114cf7e905e5c8d1
Karim <k.guezlane@gmail.com>
2022-06-23 23:11:43 +0100

    a modal component to create posts

---
+++ frontend/src/components/add post component/Addpost.jsx
@@ -0,0 +1,12 @@
+import { Container } from "../../styles/Add post";
+import PostModal from "./PostModal";
+
+function Addpost() {
+	return (
+		<Container>
+			<PostModal />
+		</Container>
+	);
+}
+
+export default Addpost;
