c4e7ef9edb53115b9bff2211a068e1512537d1c2
Karim <k.guezlane@gmail.com>
2022-06-25 21:29:56 +0100

    modal styiling

--- frontend/src/styles/Add post/addPost.styled.js
+++ frontend/src/styles/Add post/addPost.styled.js
@@ -7,7 +7,7 @@ export const Container = styled.div`
 	top: 0;
 	z-index: 100;
 	background-color: ${({ theme }) => theme.modalContainerBGC};
-	display: flex;
+	display: ${({ active }) => (active ? "flex" : "none")};
 	justify-content: center;
 	align-items: center;
 `;
@@ -46,6 +46,7 @@ export const ModalHeader = styled.div`
 		display: flex;
 		justify-content: center;
 		align-items: center;
+		cursor: pointer;
 		i {
 			font-size: 1.3em;
 		}
@@ -74,17 +75,94 @@ export const ModalProfileDiv = styled.div`
 `;
 export const ModalMediaSectionContainer = styled.div`
 	width: 100%;
+	display: flex;
+	flex-direction: column;
 	flex-grow: 1;
 	padding: 5px 15px;
-	overflow-y: scroll;
+	overflow-y: auto;
+	:hover {
+		::-webkit-scrollbar-thumb {
+			background-color: rgb(214, 214, 214);
+		}
+	}
+	::-webkit-scrollbar {
+		width: 10px;
+	}
+	::-webkit-scrollbar-track {
+		background-color: transparent;
+	}
+	::-webkit-scrollbar-thumb {
+		border-radius: 10px;
+		transition: all 1s;
+	}
 `;
 export const ModalTextarea = styled.div`
 	width: 100%;
+	position: relative;
+	flex-grow: 1;
+	cursor: text;
+`;
+export const Textarea = styled.div`
+	line-height: 30px;
+	margin-bottom: 20px;
+	outline: none;
+`;
+export const TextareaPlaceHolder = styled.div`
+	width: 100%;
+	position: absolute;
+	top: 3px;
+	h3 {
+		color: #eee;
+		font-size: 20px;
+	}
 `;
 export const ModalMediaarea = styled.div`
 	width: 100%;
 	min-height: 200px;
+	display: ${({ active }) => (active ? "flex" : "none")};
 	border: 1px solid gray;
 	border-radius: 10px;
 	padding: 5px;
+`;
+export const ModalBottomSectionContainer = styled.div`
+	width: 100%;
+	min-height: 120px;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-evenly;
+	padding: 5px 15px;
+`;
+export const AddMediaToPost = styled.div`
+	width: 100%;
+	height: 60px;
+	display: flex;
+	align-items: center;
+	justify-content: flex-start;
+	padding: 0 15px;
+	border: 1px solid grey;
+	border-radius: 10px;
+	box-shadow: ${({ theme }) => theme.mainBS};
+	h3 {
+		width: 50%;
+	}
+	i {
+		font-size: larger;
+		color: green;
+		cursor: pointer;
+	}
+`;
+export const ModalSubmitButton = styled.button`
+	width: 100%;
+	height: 40px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	padding: 0 15px;
+	border: none;
+	border-radius: 10px;
+	background-color: ${({ theme }) => theme.headerBGC};
+	h3 {
+		color: white;
+		font-weight: bold;
+	}
 `;
