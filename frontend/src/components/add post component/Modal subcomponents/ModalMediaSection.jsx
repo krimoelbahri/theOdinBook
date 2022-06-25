39976f3a7345161c381920a9f81ae591ddc10779
Karim <k.guezlane@gmail.com>
2022-06-25 21:28:17 +0100

    handeling text & media area

--- frontend/src/components/add post component/Modal subcomponents/ModalMediaSection.jsx
+++ frontend/src/components/add post component/Modal subcomponents/ModalMediaSection.jsx
@@ -1,16 +1,21 @@
+import { useRef } from "react";
 import {
 	ModalMediaSectionContainer,
 	ModalTextarea,
+	Textarea,
+	TextareaPlaceHolder,
 	ModalMediaarea,
 } from "../../../styles/Add post";
 
-function ModalMediaSection() {
+function ModalMediaSection({ media }) {
+	const textArea = useRef();
 	return (
 		<ModalMediaSectionContainer>
-			<ModalTextarea minHeight={"31px"}>
-				<div contentEditable={true}></div>
+			<ModalTextarea onClick={() => textArea.current.focus()}>
+				<Textarea contentEditable={true} role={"textbox"} ref={textArea}></Textarea>
+				<TextareaPlaceHolder>Whats on your mind {"elbahri"}</TextareaPlaceHolder>
 			</ModalTextarea>
-			<ModalMediaarea></ModalMediaarea>
+			<ModalMediaarea active={media}></ModalMediaarea>
 		</ModalMediaSectionContainer>
 	);
 }
