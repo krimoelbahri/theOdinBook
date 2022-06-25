23d8d27c9c293d0dfb35b25f4e6d07d3e65029c6
Karim <k.guezlane@gmail.com>
2022-06-25 21:26:57 +0100

    showing and hiding modal media section

--- frontend/src/components/add post component/PostModal.jsx
+++ frontend/src/components/add post component/PostModal.jsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import { ModalContainer } from "../../styles/Add post";
+import ModalbottomSection from "./Modal subcomponents/ModalbottomSection";
 import ModalMediaSection from "./Modal subcomponents/ModalMediaSection";
 import ModalTopSection from "./Modal subcomponents/ModalTopSection";
 
 function PostModal() {
+	const [media, setMedia] = useState(false);
 	return (
 		<ModalContainer>
 			<ModalTopSection />
-			<ModalMediaSection />
+			<ModalMediaSection media={media} />
+			<ModalbottomSection setMedia={setMedia} />
 		</ModalContainer>
 	);
 }
