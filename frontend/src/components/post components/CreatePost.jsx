5cb48cea1bb67c3d969a17fb9e1350d53d4e17fe
Karim <k.guezlane@gmail.com>
2022-06-25 21:28:55 +0100

    handeling showing modal

--- frontend/src/components/post components/CreatePost.jsx
+++ frontend/src/components/post components/CreatePost.jsx
@@ -1,16 +1,20 @@
 import { CreatePostContainer } from "../../styles/home.styled";
-import { useSelector } from "react-redux";
-
+import { useSelector, useDispatch } from "react-redux";
+import { showPostModal } from "../../features/Modal/modalSlice";
 function CreatePost() {
 	const { user } = useSelector((state) => state.user);
-
+	const dispatch = useDispatch();
 	return (
 		<CreatePostContainer>
 			<div className='subdiv up b-b'>
 				<img className='c-p' src={user.profilePic} alt='Pr' />
-				{user && <p className='c-p'>What's on your mind, {user.name}?</p>}
+				{user && (
+					<p className='c-p' onClick={() => dispatch(showPostModal())}>
+						What's on your mind, {user.name}?
+					</p>
+				)}
 			</div>
-			<div className='subdiv down c-p'>
+			<div className='subdiv down c-p' onClick={() => dispatch(showPostModal())}>
 				<i className='fa-solid fa-photo-film'></i>
 				<p>Photo/Video</p>
 			</div>
