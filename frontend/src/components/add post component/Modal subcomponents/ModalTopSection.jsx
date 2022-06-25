1c54e7301b7d5dbd1c8190121bd747f4fb97e773
Karim <k.guezlane@gmail.com>
2022-06-25 21:28:37 +0100

    handeling hiding modal

--- frontend/src/components/add post component/Modal subcomponents/ModalTopSection.jsx
+++ frontend/src/components/add post component/Modal subcomponents/ModalTopSection.jsx
@@ -1,11 +1,15 @@
 import { ModalHeader, ModalProfileDiv } from "../../../styles/Add post";
+import { hidePostModal } from "../../../features/Modal/modalSlice";
+import { useDispatch } from "react-redux";
 
 function ModalTopSection() {
+	const dispatch = useDispatch();
+
 	return (
 		<div className='top-section'>
 			<ModalHeader className='b-b'>
 				<h2>Create Post</h2>
-				<span className='item'>
+				<span className='item' onClick={() => dispatch(hidePostModal())}>
 					<i className='fa-solid fa-xmark'></i>
 				</span>
 			</ModalHeader>
