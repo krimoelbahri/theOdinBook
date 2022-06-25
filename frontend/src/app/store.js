35754d4de2068332b07021e444fdf7dd93e9b592
Karim <k.guezlane@gmail.com>
2022-06-25 21:25:13 +0100

    importing modal reducer

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/auth/userSlice";
 import dropDownReducer from "../features/dropDown/dropDownSlice";
-
+import modalReducer from "../features/Modal/modalSlice";
 export const store = configureStore({
 	reducer: {
 		user: userReducer,
 		dropDown: dropDownReducer,
+		modal: modalReducer,
 	},
 });
