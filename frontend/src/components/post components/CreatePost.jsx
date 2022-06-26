67b2eac3ada31e6b24c92999d48b0e713a20db45
Karim <k.guezlane@gmail.com>
2022-06-26 18:10:38 +0100

    handeling post modal

--- frontend/src/components/post components/CreatePost.jsx
+++ frontend/src/components/post components/CreatePost.jsx
@@ -1,6 +1,6 @@
 import { CreatePostContainer } from "../../styles/home.styled";
 import { useSelector, useDispatch } from "react-redux";
-import { showPostModal } from "../../features/Modal/modalSlice";
+import { handlePostModal } from "../../features/Modal/modalSlice";
 function CreatePost() {
 	const { user } = useSelector((state) => state.user);
 	const dispatch = useDispatch();
@@ -9,12 +9,12 @@ function CreatePost() {
 			<div className='subdiv up b-b'>
 				<img className='c-p' src={user.profilePic} alt='Pr' />
 				{user && (
-					<p className='c-p' onClick={() => dispatch(showPostModal())}>
+					<p className='c-p' onClick={() => dispatch(handlePostModal(true))}>
 						What's on your mind, {user.name}?
 					</p>
 				)}
 			</div>
-			<div className='subdiv down c-p' onClick={() => dispatch(showPostModal())}>
+			<div className='subdiv down c-p' onClick={() => dispatch(handlePostModal(true))}>
 				<i className='fa-solid fa-photo-film'></i>
 				<p>Photo/Video</p>
 			</div>
