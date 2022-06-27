c6861478328cfb5d89c7ad0a7fa85615bcb80921
Karim <k.guezlane@gmail.com>
2022-06-27 22:55:10 +0100

    added Post reducer

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -2,9 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/auth/userSlice";
 import dropDownReducer from "../features/dropDown/dropDownSlice";
 import modalReducer from "../features/Modal/modalSlice";
+import postReducer from "../features/posts/postSlice";
 export const store = configureStore({
 	reducer: {
 		user: userReducer,
+		post: postReducer,
 		dropDown: dropDownReducer,
 		modal: modalReducer,
 	},
