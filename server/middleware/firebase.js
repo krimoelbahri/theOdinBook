bf2744e66912ae5483dfe8826dfd84763ec8d9d1
Karim <k.guezlane@gmail.com>
2022-06-28 21:49:55 +0100

    initializing multer firebase

--- server/middleware/firebase.js
+++ server/middleware/firebase.js
@@ -1,17 +1,17 @@
-const { initializeApp } = require("firebase/app");
-const { getStorage, uploadBytes, ref, getDownloadURL } = require("firebase/storage");
+const Multer = require("multer");
+const FirebaseStorage = require("multer-firebase-storage");
 
-const firebaseConfig = {
-	apiKey: process.env.APIKEY,
-	authDomain: process.env.AUTHDOMAIN,
-	storageBucket: process.env.STORAGEBUCKET,
-};
-const firebaseApp = initializeApp(firebaseConfig);
-const storage = getStorage(firebaseApp);
-function uploadImages(reference, file) {
-	return uploadBytes(ref(storage, reference), file);
-}
-function DownloadImages(reference) {
-	return getDownloadURL(ref(storage, reference));
-}
-module.exports = { uploadImages, DownloadImages };
+const multer = Multer({
+	storage: FirebaseStorage({
+		bucketName: process.env.STORAGEBUCKET,
+		credentials: {
+			clientEmail: process.env.CLIENTEMAIL,
+			privateKey: process.env.PRIVATEKEY.replace(/\\n/g, "\n"),
+			projectId: process.env.PROJECTID,
+		},
+		directoryPath: "post",
+		public: true,
+	}),
+});
+
+module.exports = { multer };
