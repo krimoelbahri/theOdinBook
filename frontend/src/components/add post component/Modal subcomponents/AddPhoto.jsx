1d89bef6980ab1d938a165bbfb95840fa784dae9
Karim <k.guezlane@gmail.com>
2022-06-28 21:46:29 +0100

    adding photo to data object

--- frontend/src/components/add post component/Modal subcomponents/AddPhoto.jsx
+++ frontend/src/components/add post component/Modal subcomponents/AddPhoto.jsx
@@ -1,12 +1,32 @@
+import { useRef } from "react";
 import { AddPhotoContainer, AddPhotoInput } from "../../../styles/Add post";
 
-function AddPhoto() {
+function AddPhoto({ url, setUrl, setData }) {
+	const fileInput = useRef();
+	function handleFileInput() {
+		fileInput.current.click();
+	}
+	function handleImage() {
+		let selectedImage = fileInput.current.files[0];
+		let url = URL.createObjectURL(selectedImage);
+		setUrl(url);
+		setData((state) => ({ ...state, imgFile: selectedImage }));
+	}
 	return (
 		<AddPhotoContainer>
-			<AddPhotoInput>
-				<i className='fa-solid fa-file-medical'></i>
-				<h1>Add Photos</h1>
-			</AddPhotoInput>
+			{!url && (
+				<AddPhotoInput onClick={handleFileInput}>
+					<i className='fa-solid fa-file-medical'></i>
+					<h1>Add Photos</h1>
+				</AddPhotoInput>
+			)}
+			{url && <img src={url} alt={"holder"} />}
+			<input
+				onChange={handleImage}
+				type={"file"}
+				style={{ display: "none" }}
+				ref={fileInput}
+			/>
 		</AddPhotoContainer>
 	);
 }
