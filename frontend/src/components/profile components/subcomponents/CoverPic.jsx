55042aef486732bb7aef9f8d89e1eb078cf8d0c7
Karim <k.guezlane@gmail.com>
2022-06-30 22:22:55 +0100

    passing cover pic

--- frontend/src/components/profile components/subcomponents/CoverPic.jsx
+++ frontend/src/components/profile components/subcomponents/CoverPic.jsx
@@ -1,11 +1,7 @@
-import { useSelector } from "react-redux";
-
-function CoverPic() {
-	const { user } = useSelector((state) => state.user);
-
+function CoverPic({ user }) {
 	return (
 		<div className='wrapper'>
-			<img src={user.coverPic} alt='Cover' />
+			<img src={user?.coverPic} alt='Cover' />
 			<button>Edit Cover Photo</button>
 			<div></div> {/*TODO: Edit DD */}
 		</div>
