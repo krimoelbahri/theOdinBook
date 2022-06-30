196df0b2e40cdc1714fcb8c7cbdd201be2c1daaa
Karim <k.guezlane@gmail.com>
2022-06-30 22:23:09 +0100

    passing profile infos

--- frontend/src/components/profile components/subcomponents/ProfileInfo.jsx
+++ frontend/src/components/profile components/subcomponents/ProfileInfo.jsx
@@ -1,16 +1,13 @@
-import { useSelector } from "react-redux";
 import { ProfilePicWrapper, ProfileName, ProfilButtons } from "../../../styles/profile";
 
-function ProfileInfo() {
-	const { user } = useSelector((state) => state.user);
-
+function ProfileInfo({ user }) {
 	return (
 		<div className='wrapper'>
 			<ProfilePicWrapper>
-				<img src={user.profilePic} alt='' />
+				<img src={user?.profilePic} alt='' />
 			</ProfilePicWrapper>
 			<ProfileName>
-				<h1>{user.name}</h1>
+				<h1>{user?.name}</h1>
 			</ProfileName>
 			<ProfilButtons>
 				<button>Edit Profile</button>
