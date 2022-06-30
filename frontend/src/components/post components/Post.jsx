16bf0e82806fb3714a61aaf56e8a142ef6cfb7c1
Karim <k.guezlane@gmail.com>
2022-06-30 22:17:10 +0100

    passing props to deffirent post components

--- frontend/src/components/post components/Post.jsx
+++ frontend/src/components/post components/Post.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { PostContainer } from "../../styles/Post.styled";
 import { PostHeader, PostComments, PostDescription, PostImg, PostReactions } from "./subcomponents";
-function Post() {
+function Post({ post }) {
 	return (
 		<PostContainer>
-			<PostHeader />
-			<PostDescription />
-			<PostImg />
-			<PostReactions />
-			<PostComments />
+			<PostHeader user={post?.author} date={post?.createdAt} />
+			<PostDescription description={post?.description} />
+			<PostImg url={post?.postImage} />
+			<PostReactions comments={post?.comments} likes={post?.likes} />
+			<PostComments comments={post?.comments} />
 		</PostContainer>
 	);
 }
