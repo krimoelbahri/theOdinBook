a93878551b39562cfd7c9925a5d8cb4a3c086709
Karim <k.guezlane@gmail.com>
2022-07-01 22:02:10 +0100

    handling RtL text

--- frontend/src/components/add post component/Modal subcomponents/ModalMediaSection.jsx
+++ frontend/src/components/add post component/Modal subcomponents/ModalMediaSection.jsx
@@ -1,4 +1,6 @@
 import { useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import isRTL from "../../../helpers/isRTL";
 import {
 	ModalMediaSectionContainer,
 	ModalTextarea,
@@ -7,17 +9,22 @@ import {
 	ModalMediaarea,
 } from "../../../styles/Add post";
 import AddPhoto from "./AddPhoto";
+
 function ModalMediaSection({ media, setMedia, url, setUrl, setData }) {
 	const textArea = useRef();
 	const [placeHolder, setPlaceHolder] = useState(true);
+	const [rtl, setRtl] = useState("");
+	const { user } = useSelector((state) => state.user);
 	function handleChange(e) {
-		if (e.target.textContent === "") {
+		if (e.target.innerText === "") {
+			setRtl("");
 			setPlaceHolder(true);
 			setData((state) => ({ ...state, description: null }));
 		}
-		if (e.target.textContent !== "") {
+		if (e.target.innerText !== "") {
+			if (isRTL(e.target.textContent)) setRtl("rtl");
 			setPlaceHolder(false);
-			setData((state) => ({ ...state, description: e.target.textContent }));
+			setData((state) => ({ ...state, description: e.target.innerText }));
 		}
 	}
 	return (
@@ -28,9 +35,10 @@ function ModalMediaSection({ media, setMedia, url, setUrl, setData }) {
 					contentEditable={true}
 					role={"textbox"}
 					ref={textArea}
+					dir={rtl}
 				></Textarea>
 				{placeHolder && (
-					<TextareaPlaceHolder>Whats on your mind {"elbahri"}</TextareaPlaceHolder>
+					<TextareaPlaceHolder>Whats on your mind, {user.name} ?</TextareaPlaceHolder>
 				)}
 			</ModalTextarea>
 			<ModalMediaarea active={media}>
