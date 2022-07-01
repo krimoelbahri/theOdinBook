3e7c8feda06b62c0b58ba14e1db677bab2da1080
Karim <k.guezlane@gmail.com>
2022-07-01 22:03:06 +0100

    showing proper text and handling RtL text

--- frontend/src/components/post components/subcomponents/PostDescription.jsx
+++ frontend/src/components/post components/subcomponents/PostDescription.jsx
@@ -1,9 +1,18 @@
+import { useEffect, useState, useRef } from "react";
+import isRTL from "../../../helpers/isRTL";
 import { PostDescriptionContainer } from "../../../styles/Post.styled";
 
 function PostDescription({ description }) {
+	const [rtl, setRtl] = useState("");
+	const paragraph = useRef();
+	useEffect(() => {
+		if (isRTL(description)) setRtl("rtl");
+		paragraph.current.innerText = description;
+	}, [description]);
+
 	return (
-		<PostDescriptionContainer>
-			<p> {description}</p>
+		<PostDescriptionContainer dir={rtl}>
+			<p ref={paragraph}> </p>
 		</PostDescriptionContainer>
 	);
 }
