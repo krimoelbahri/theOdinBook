e4dc7cddbbffd9baa2b9535ccc4a7a3cf71f06b0
Karim <k.guezlane@gmail.com>
2022-07-06 20:38:23 +0100

    showing sugested cards

--- frontend/src/pages/Friends.jsx
+++ frontend/src/pages/Friends.jsx
@@ -1,17 +1,44 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import { Card } from "../components/friends components";
 import { Container, FriendsHeader, FriendsCardsContainer } from "../styles/friends";
-
+import { useDispatch, useSelector } from "react-redux";
+import { getUsers } from "../features/auth/userSlice";
 function Friends() {
+	const [users, setUsers] = useState([]);
+	const { user } = useSelector((state) => state.user);
+	const [friends, setFriends] = useState(user.friends);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		async function fetchUsers() {
+			try {
+				let users = await dispatch(getUsers()).unwrap();
+				setUsers(users);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		fetchUsers();
+	}, [dispatch]);
+
 	return (
 		<Container>
 			<FriendsHeader>
 				<h1>People you may know</h1>
 			</FriendsHeader>
 			<FriendsCardsContainer>
-				<Card />
-				<Card />
-				<Card />
-				<Card />
+				{users
+					.filter(({ _id }) => _id !== user._id)
+					.filter(({ _id }) => friends?.every((friend) => friend._id !== _id))
+					.map((user) => (
+						<Card
+							key={`card${user._id}`}
+							user={user}
+							setFriends={setFriends}
+							setUsers={setUsers}
+						/>
+					))}
 			</FriendsCardsContainer>
 		</Container>
 	);
