ca2e51a67fa2e26508dec0cef1a3388676bc2561
Karim <k.guezlane@gmail.com>
2022-07-06 12:01:47 +0100

    text color, & linking to user profile page

--- frontend/src/components/post components/CreatePost.jsx
+++ frontend/src/components/post components/CreatePost.jsx
@@ -1,22 +1,27 @@
 import { CreatePostContainer } from "../../styles/home.styled";
 import { useSelector, useDispatch } from "react-redux";
 import { handlePostModal } from "../../features/Modal/modalSlice";
+import { Link } from "react-router-dom";
+
 function CreatePost() {
 	const { user } = useSelector((state) => state.user);
 	const dispatch = useDispatch();
 	return (
 		<CreatePostContainer>
 			<div className='subdiv up b-b'>
-				<img className='c-p' src={user.profilePic} alt='Pr' />
+				<Link to={`/${user._id}`}>
+					<img className='c-p' src={user.profilePic} alt='Pr' />
+				</Link>
+
 				{user && (
 					<p className='c-p' onClick={() => dispatch(handlePostModal(true))}>
-						What's on your mind, {user.name}?
+						What's on your mind, <span className='bold'>{user.name}</span> ?
 					</p>
 				)}
 			</div>
 			<div className='subdiv down c-p' onClick={() => dispatch(handlePostModal(true))}>
 				<i className='fa-solid fa-photo-film'></i>
-				<p>Photo/Video</p>
+				<p className='bold'>Photo/Video</p>
 			</div>
 		</CreatePostContainer>
 	);
