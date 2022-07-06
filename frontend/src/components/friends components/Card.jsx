3cb32bcb699df683ad6f1303b86cb48b424ff0c1
Karim <k.guezlane@gmail.com>
2022-07-06 20:32:22 +0100

    setting up suggesting friends cards

--- frontend/src/components/friends components/Card.jsx
+++ frontend/src/components/friends components/Card.jsx
@@ -1,18 +1,26 @@
 import { CardContainer, CardButton } from "../../styles/friends";
 
-export default function Card() {
+export default function Card({ user, setFriends, setUsers }) {
+	function handleAddfriends() {}
+	function handleRemoveCard() {
+		setUsers((state) => state.filter((cardUser) => cardUser._id !== user._id));
+	}
+
 	return (
 		<CardContainer>
-			<img
-				src='https://images.unsplash.com/photo-1614632537190-23e4146777db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-				alt='Ball'
+			<img src={user?.profilePic} alt='Ball' className='m-b-10' />
+			<h2 className='m-b-10'>{user?.name}</h2>
+			<CardButton
+				onClick={handleAddfriends}
 				className='m-b-10'
-			/>
-			<h2 className='m-b-10'>Profile Name</h2>
-			<CardButton className='m-b-10' fontColor={"#1877F2"} bgColor={"#E7F3FF"}>
+				fontColor={"#1877F2"}
+				bgColor={"#E7F3FF"}
+			>
 				Add friend
 			</CardButton>
-			<CardButton className='m-b-10'>Remove</CardButton>
+			<CardButton className='m-b-10' onClick={handleRemoveCard}>
+				Remove
+			</CardButton>
 		</CardContainer>
 	);
 }
