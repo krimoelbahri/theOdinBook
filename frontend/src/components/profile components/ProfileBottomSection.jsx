39d47659252f1707760558264e85d6a90b4a2c80
Karim <k.guezlane@gmail.com>
2022-07-06 20:35:58 +0100

    showing friends in profile

--- frontend/src/components/profile components/ProfileBottomSection.jsx
+++ frontend/src/components/profile components/ProfileBottomSection.jsx
@@ -14,21 +14,18 @@ function ProfileBottomSection({ currentUser, post, user }) {
 				<ProfileLeftBar>
 					<FriendsContainer>
 						<div className='header'>
-							<Link to={`/${user._id}?sf=friends`} state={{ data: "friends" }}>
+							<Link to={`/${user?._id}?sf=friends`} state={{ data: "friends" }}>
 								<h3>Friends</h3>
 							</Link>
-							<Link to={`/${user._id}?sf=friends`} state={{ data: "friends" }}>
+							<Link to={`/${user?._id}?sf=friends`} state={{ data: "friends" }}>
 								<h4>See all friends</h4>
 							</Link>
 						</div>
 						<div className='wrapper'>
-							{Array.from(Array(9)).map((_, i) => (
-								<div key={`card${i}`} className='card'>
-									<img
-										src='https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fA%3D%3D&w=1000&q=80'
-										alt='card'
-									/>
-									<h3>Profile Name</h3>
+							{user?.friends?.map((friend, i) => (
+								<div key={`card${friend._id}`} className='card'>
+									<img src={friend.profilePic} alt='card' />
+									<h3>{friend.name}</h3>
 								</div>
 							))}
 						</div>
@@ -36,8 +33,8 @@ function ProfileBottomSection({ currentUser, post, user }) {
 				</ProfileLeftBar>
 				<ProfilePostsWrapper>
 					{currentUser && <CreatePost />}
-					{post?.posts?.map((post) => (
-						<Post key={post._id} post={post} />
+					{post?.posts?.map((post, i) => (
+						<Post key={post._id} post={post} postIndex={i} />
 					))}
 				</ProfilePostsWrapper>
 			</div>
