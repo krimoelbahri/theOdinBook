21504be5971a38ab657177d642274c9ba2a49105
Karim <k.guezlane@gmail.com>
2022-07-06 20:53:08 +0100

    get users controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,6 +3,7 @@ const passport = require("passport");
 const User = require("../models/user");
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
+const user = require("../models/user");
 
 // Generate JWT
 const generateToken = (id) => {
@@ -10,22 +11,39 @@ const generateToken = (id) => {
 		expiresIn: "30d",
 	});
 };
+
+//Get User by Id
 exports.getUser = asyncHandler(async function (req, res) {
-	let id = req.params.id;
-	const user = await User.findById(id);
-	if (!user) {
+	try {
+		let id = req.params.id;
+		const user = await User.findById(id, "name profilePic coverPic friends").populate({
+			path: "friends",
+			model: "User",
+			select: "name profilePic",
+		});
+		res.status(200).json(user);
+	} catch (error) {
 		res.status(400);
-		throw new Error("No user found");
+		throw new Error(error);
 	}
-	res.status(200).json({
-		_id: user.id,
-		name: user.name,
-		email: user.email,
-		profilePic: user.profilePic,
-		coverPic: user.coverPic,
-	});
 });
 
+//Get all users
+exports.getUsers = asyncHandler(async function (req, res) {
+	try {
+		const users = await User.find({}, "name profilePic friends").populate({
+			path: "friends",
+			model: "User",
+			select: "name profilePic",
+		});
+		res.status(200).json(users);
+	} catch (error) {
+		res.status(400);
+		throw new Error(error);
+	}
+});
+
+//Signup
 exports.signupUser = asyncHandler(async function (req, res) {
 	const { name, email, password, confirmPassword } = req.body;
 	if (!name || !email || !password || !confirmPassword) {
@@ -63,6 +81,7 @@ exports.signupUser = asyncHandler(async function (req, res) {
 			email: user.email,
 			profilePic: user.profilePic,
 			coverPic: user.coverPic,
+			friends: user.friends,
 			token: generateToken(user.id),
 		});
 	} else {
@@ -92,11 +111,14 @@ exports.localSigninUser = function (req, res, next) {
 				email: req.user.email,
 				profilePic: user.profilePic,
 				coverPic: user.coverPic,
+				friends: user.friends,
 				token: generateToken(req.user.id),
 			});
 		});
 	})(req, res, next);
 };
+
+//facebook signin logic
 exports.facebookSigninUser = passport.authenticate("facebook");
 exports.facebookSigninUserCB = function (req, res, next) {
 	passport.authenticate("facebook", function (err, user, info) {
