cb924f48423eec73b25b00ffcb802d3bdddafd41
Karim <k.guezlane@gmail.com>
2022-07-06 20:35:37 +0100

    exporting Cp modal and Pp modal

--- frontend/src/components/profile components/index.js
+++ frontend/src/components/profile components/index.js
@@ -1,5 +1,13 @@
+import CoverPicModal from "./CoverPicModal";
 import ProfileBottomSection from "./ProfileBottomSection";
 import ProfileNavSection from "./ProfileNavSection";
+import ProfilePicModal from "./ProfilePicModal";
 import ProfileTopSection from "./ProfileTopSection";
 
-export { ProfileTopSection, ProfileNavSection, ProfileBottomSection };
+export {
+	ProfileTopSection,
+	ProfileNavSection,
+	ProfileBottomSection,
+	ProfilePicModal,
+	CoverPicModal,
+};
