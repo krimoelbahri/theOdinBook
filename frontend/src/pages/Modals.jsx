3f4c61a728bdc5ec2dfcc98a4ecc8ca2eaf74425
Karim <k.guezlane@gmail.com>
2022-07-06 20:51:51 +0100

    Modal page

---
+++ frontend/src/pages/Modals.jsx
@@ -0,0 +1,19 @@
+import { Container } from "../styles/Add post";
+import PostModal from "../components/add post component";
+import { useSelector } from "react-redux";
+import { CoverPicModal, ProfilePicModal } from "../components/profile components";
+
+function Modals() {
+	const { postModal, coverPicModal, profilePicModal } = useSelector((state) => state.modal);
+	const { user } = useSelector((state) => state.user);
+
+	return (
+		<>
+			{postModal && <Container>{user && <PostModal />}</Container>}
+			{coverPicModal && <Container>{user && <CoverPicModal />}</Container>}
+			{profilePicModal && <Container>{user && <ProfilePicModal />}</Container>}
+		</>
+	);
+}
+
+export default Modals;
