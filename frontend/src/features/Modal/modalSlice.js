56228e9dc25c355187c3840f2b8a408aed762f25
Karim <k.guezlane@gmail.com>
2022-07-06 20:37:40 +0100

    handeling multiple modals

--- frontend/src/features/Modal/modalSlice.js
+++ frontend/src/features/Modal/modalSlice.js
@@ -1,13 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 let initialState = {
 	postModal: false,
+	coverPicModal: false,
+	profilePicModal: false,
 };
 let modalSlice = createSlice({
 	name: "Modal",
 	initialState,
 	reducers: {
+		resetModals: (state) => {
+			state = initialState;
+		},
 		handlePostModal: (state, actions) => {
 			state.postModal = actions.payload;
+		},
+		handleCPModal: (state, actions) => {
+			state.coverPicModal = actions.payload;
+		},
+		handlePPModal: (state, actions) => {
+			state.profilePicModal = actions.payload;
 		},
 	},
 });
