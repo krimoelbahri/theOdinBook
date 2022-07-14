064e8bd06b5aa00fe91bed9df4a1b9eb40b7ebb2
Karim <k.guezlane@gmail.com>
2022-07-14 11:18:50 +0100

    showing skeleton when loading img

--- frontend/src/components/profile components/subcomponents/ProfileInfo.jsx
+++ frontend/src/components/profile components/subcomponents/ProfileInfo.jsx
@@ -1,10 +1,19 @@
 import { ProfilePicWrapper, ProfileName, ProfilButtons } from "../../../styles/profile";
-
+import { Skeleton } from "@mantine/core";
+import { useState } from "react";
 function ProfileInfo({ user }) {
+	const [loaded, setloaded] = useState(false);
 	return (
 		<div className='wrapper'>
-			<ProfilePicWrapper>
-				<img src={user?.profilePic} alt='' />
+			<ProfilePicWrapper visible={loaded}>
+				<Skeleton
+					visible={!loaded}
+					height={190}
+					style={{ position: "absolute", left: "5px", top: "5px" }}
+					circle
+					mb='xl'
+				/>
+				<img onLoad={() => setloaded(true)} src={user?.profilePic} alt='' />
 			</ProfilePicWrapper>
 			<ProfileName>
 				<h1>{user?.name}</h1>
