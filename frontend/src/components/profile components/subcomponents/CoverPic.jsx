0f8f8a6cdf7a762637dcf5b1deb184efa7b99fdb
Karim <k.guezlane@gmail.com>
2022-07-15 14:00:49 +0100

    styling editing button

--- frontend/src/components/profile components/subcomponents/CoverPic.jsx
+++ frontend/src/components/profile components/subcomponents/CoverPic.jsx
@@ -7,7 +7,10 @@ function CoverPic({ user }) {
 	return (
 		<Skeleton visible={!loaded} className='wrapper'>
 			<img onLoad={() => setloaded(true)} src={user?.coverPic} alt='Cover' />
-			<button>Edit Cover Photo</button>
+			<button>
+				<i className='fa-solid fa-camera'></i>
+				Edit Cover Photo
+			</button>
 			<div></div> {/*TODO: Edit DD */}
 		</Skeleton>
 	);
