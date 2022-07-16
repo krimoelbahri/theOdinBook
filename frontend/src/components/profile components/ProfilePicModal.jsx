cda7ac6ba5551fbe613776ae01468482148d3af9
Karim <k.guezlane@gmail.com>
2022-07-16 20:29:28 +0100

    handling updating profile picture

--- frontend/src/components/profile components/ProfilePicModal.jsx
+++ frontend/src/components/profile components/ProfilePicModal.jsx
@@ -1,7 +1,64 @@
-import React from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+	ModalContainer,
+	ModalBottomSectionContainer,
+	ModalMediaSectionContainer,
+	ModalMediaarea,
+	ModalSubmitButton,
+	ModalHeader,
+} from "../../styles/Modals";
+import { handlePPModal } from "../../features/Modal/modalSlice";
+import AddPhoto from "../add post component/Modal subcomponents/AddPhoto";
+import { updateImage, updateUser } from "../../features/auth/userSlice";
 
-function ProfilePicModal() {
-	return <div>ProfilePicModal</div>;
+function PostModal() {
+	//using Redux
+	const dispatch = useDispatch();
+	const { user } = useSelector((state) => state.user);
+	//useState state handeling post data
+	const [url, setUrl] = useState(null);
+	const [data, setData] = useState({ action: "profile", author: user._id, imgFile: null });
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+		try {
+			let user = await dispatch(updateImage(data)).unwrap();
+			console.log(user);
+			dispatch(updateUser(user));
+			dispatch(handlePPModal(false));
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	return (
+		<ModalContainer onSubmit={handleSubmit}>
+			<div className='top-section'>
+				<ModalHeader className='b-b'>
+					<h2>Update Profile Picture</h2>
+					<span className='item' onClick={() => dispatch(handlePPModal(false))}>
+						<i className='fa-solid fa-xmark'></i>
+					</span>
+				</ModalHeader>
+			</div>
+			<ModalMediaSectionContainer>
+				<ModalMediaarea active={true}>
+					<AddPhoto url={url} setUrl={setUrl} setData={setData} />
+				</ModalMediaarea>
+			</ModalMediaSectionContainer>
+			<ModalBottomSectionContainer>
+				<ModalSubmitButton type='submit'>
+					<h3>Update Picture</h3>
+				</ModalSubmitButton>
+			</ModalBottomSectionContainer>
+			{/*addPost.isLoading && (
+				<Loader>
+					<i className='fa-solid fa-spinner fa-spin-pulse'></i>
+				</Loader>
+			)*/}
+		</ModalContainer>
+	);
 }
 
-export default ProfilePicModal;
+export default PostModal;
