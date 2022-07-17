fe00d8a02b499c10696f4bc67d2a12ada78cfe7a
Karim <k.guezlane@gmail.com>
2022-07-17 18:01:46 +0100

    deleting only if it's your post

--- frontend/src/components/post components/subcomponents/PostHeader.jsx
+++ frontend/src/components/post components/subcomponents/PostHeader.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import { useClickOutside } from "@mantine/hooks";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { deletePost, removePost } from "../../../features/posts/postSlice";
 import { PostHeaderContainer, PostHeaderDD, ProfileDiv } from "../../../styles/Post.styled";
 
-function PostHeader({ user, date, postId, postIndex }) {
+function PostHeader({ author, date, postId, postIndex }) {
 	const DD = useClickOutside(() => setIsActive(false));
 	const dispatch = useDispatch();
-
+	const { user } = useSelector((state) => state.user);
 	const [isActive, setIsActive] = useState(false);
 	const [isDeleting, setIsDeleting] = useState(false);
 
@@ -25,39 +25,41 @@ function PostHeader({ user, date, postId, postIndex }) {
 	return (
 		<PostHeaderContainer>
 			<ProfileDiv>
-				<Link to={`/${user._id}`}>
-					<img className='c-p' src={user?.profilePic.url} alt='' />
+				<Link to={`/${author._id}`}>
+					<img className='c-p' src={author?.profilePic.url} alt='' />
 				</Link>
 				<div>
-					<Link to={`/${user._id}`}>
-						<p className='name c-p'>{user?.name}</p>
+					<Link to={`/${author._id}`}>
+						<p className='name c-p'>{author?.name}</p>
 					</Link>
 					<p className='date c-p'>{date}</p>
 				</div>
 			</ProfileDiv>
-			<div style={{ position: "relative" }} ref={DD}>
-				{!isDeleting && (
-					<>
-						<div
-							className='icon c-p'
-							onClick={() => (!isActive ? setIsActive(true) : setIsActive(false))}
-						>
-							<i className='fa-solid fa-ellipsis'></i>
-						</div>
-						<PostHeaderDD active={isActive}>
-							<div className='c-p' onClick={handleDeletePost}>
-								<i className='fa-solid fa-trash-can '></i>
-								<p>Delete</p>
+			{user._id === author._id && (
+				<div style={{ position: "relative" }} ref={DD}>
+					{!isDeleting && (
+						<>
+							<div
+								className='icon c-p'
+								onClick={() => (!isActive ? setIsActive(true) : setIsActive(false))}
+							>
+								<i className='fa-solid fa-ellipsis'></i>
 							</div>
-						</PostHeaderDD>
-					</>
-				)}
-				{isDeleting && (
-					<div className='icon'>
-						<i className='fa-solid fa-spinner fa-spin-pulse'></i>
-					</div>
-				)}
-			</div>
+							<PostHeaderDD active={isActive}>
+								<div className='c-p' onClick={handleDeletePost}>
+									<i className='fa-solid fa-trash-can '></i>
+									<p>Delete</p>
+								</div>
+							</PostHeaderDD>
+						</>
+					)}
+					{isDeleting && (
+						<div className='icon'>
+							<i className='fa-solid fa-spinner fa-spin-pulse'></i>
+						</div>
+					)}
+				</div>
+			)}
 		</PostHeaderContainer>
 	);
 }
