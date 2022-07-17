cadb1fe6c3360b91616b8033050d9d236292924c
Karim <k.guezlane@gmail.com>
2022-07-17 18:03:08 +0100

    updating only if your profile

--- frontend/src/components/profile components/subcomponents/CoverPic.jsx
+++ frontend/src/components/profile components/subcomponents/CoverPic.jsx
@@ -1,16 +1,21 @@
 import { Skeleton } from "@mantine/core";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { handleCPModal } from "../../../features/Modal/modalSlice";
 
-function CoverPic({ user }) {
+function CoverPic({ currentUser, user }) {
+	const dispatch = useDispatch();
 	const [loaded, setloaded] = useState(false);
 
 	return (
 		<Skeleton visible={!loaded} className='wrapper'>
 			<img onLoad={() => setloaded(true)} src={user?.coverPic.url} alt='Cover' />
-			<button>
-				<i className='fa-solid fa-camera'></i>
-				Edit Cover Photo
-			</button>
+			{currentUser && (
+				<button onClick={() => dispatch(handleCPModal(true))}>
+					<i className='fa-solid fa-camera'></i>
+					Edit Cover Photo
+				</button>
+			)}
 			<div></div> {/*TODO: Edit DD */}
 		</Skeleton>
 	);
