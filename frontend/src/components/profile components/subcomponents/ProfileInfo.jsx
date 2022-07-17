f8d09350626e66ddbba7cc6fa15daa8fd4bb5f7a
Karim <k.guezlane@gmail.com>
2022-07-17 18:03:14 +0100

    updating only if your profile

--- frontend/src/components/profile components/subcomponents/ProfileInfo.jsx
+++ frontend/src/components/profile components/subcomponents/ProfileInfo.jsx
@@ -4,31 +4,36 @@ import { useState } from "react";
 import { handlePPModal } from "../../../features/Modal/modalSlice";
 import { useDispatch } from "react-redux";
 
-function ProfileInfo({ user }) {
+function ProfileInfo({ currentUser, user }) {
 	const dispatch = useDispatch();
 	const [loaded, setloaded] = useState(false);
+
 	return (
 		<div className='wrapper'>
 			<ProfilePicWrapper visible={loaded}>
 				<Skeleton
 					visible={!loaded}
 					height={170}
-					style={{ position: "absolute", left: "5px", top: "5px" }}
+					style={{ position: "absolute" }}
 					circle
 					mb='xl'
 				/>
 				<img onLoad={() => setloaded(true)} src={user?.profilePic.url} alt='' />
-				<div className='edit-icon c-p' onClick={() => dispatch(handlePPModal(true))}>
-					<i className='fa-solid fa-camera'></i>
-				</div>
+				{currentUser && (
+					<div className='edit-icon c-p' onClick={() => dispatch(handlePPModal(true))}>
+						<i className='fa-solid fa-camera'></i>
+					</div>
+				)}
 			</ProfilePicWrapper>
 			<ProfileName>
 				<h1>{user?.name}</h1>
 			</ProfileName>
 			<ProfilButtons>
-				<button>
-					<i className='fa-solid fa-pen'></i>Edit Profile
-				</button>
+				{currentUser && (
+					<button>
+						<i className='fa-solid fa-pen'></i>Edit Profile
+					</button>
+				)}
 			</ProfilButtons>
 		</div>
 	);
