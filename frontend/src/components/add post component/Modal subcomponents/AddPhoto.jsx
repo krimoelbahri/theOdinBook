63fa5a9a4047ba5b8bd01d6737e645248bd51366
Karim <k.guezlane@gmail.com>
2022-07-19 18:27:10 +0100

    validating images

--- frontend/src/components/add post component/Modal subcomponents/AddPhoto.jsx
+++ frontend/src/components/add post component/Modal subcomponents/AddPhoto.jsx
@@ -1,15 +1,36 @@
 import { useRef } from "react";
+import { validateImgWidth } from "../../../helpers/validateImg";
 import { AddPhotoContainer, AddPhotoInput } from "../../../styles/Modals";
 
-function AddPhoto({ url, setUrl, setData }) {
+function AddPhoto({ action, url, setUrl, setData }) {
 	const fileInput = useRef();
 
 	function handleFileInput() {
 		if (!url) fileInput.current.click();
 	}
 
-	function handleImage() {
+	async function handleImage() {
 		let selectedImage = fileInput.current.files[0];
+		if (
+			selectedImage.type.split("/")[1] !== "png" &&
+			selectedImage.type.split("/")[1] !== "webp" &&
+			selectedImage.type.split("/")[1] !== "jpeg" &&
+			selectedImage.type.split("/")[1] !== "jpg"
+		) {
+			//TODO: handle error if non-Image file was selected
+			return;
+		}
+		if (selectedImage.size > 1000000) {
+			console.log("file too big");
+			return;
+		}
+		if (action === "cover") {
+			let test = await validateImgWidth(selectedImage);
+			if (!test) {
+				//TODO: handle cover image width & height errors
+				return;
+			}
+		}
 		let url = URL.createObjectURL(selectedImage);
 		setUrl(url);
 		setData((state) => ({ ...state, imgFile: selectedImage }));
