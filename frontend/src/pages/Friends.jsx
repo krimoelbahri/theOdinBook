db85e90d05fd8d016fc5867911cd2c88ce1fccd9
Karim <k.guezlane@gmail.com>
2022-07-21 17:18:54 +0100

    handle friends Cards

--- frontend/src/pages/Friends.jsx
+++ frontend/src/pages/Friends.jsx
@@ -3,22 +3,12 @@ import { useState } from "react";
 import { Card } from "../components/friends components";
 import { Container, FriendsHeader, FriendsCardsContainer } from "../styles/friends";
 import { useDispatch, useSelector } from "react-redux";
-import { getUsers, addFriend, updateUser } from "../features/auth/userSlice";
+import { getUsers } from "../features/auth/userSlice";
+
 function Friends() {
 	const [users, setUsers] = useState([]);
 	const { user } = useSelector((state) => state.user);
-	const [friends, setFriends] = useState(user.friends);
 	const dispatch = useDispatch();
-
-	async function handleAddfriends(id) {
-		try {
-			let updatedUser = await dispatch(addFriend({ author: user._id, friend: id })).unwrap();
-			dispatch(updateUser(updatedUser));
-			setFriends(updatedUser.friends);
-		} catch (error) {
-			console.log(error);
-		}
-	}
 
 	useEffect(() => {
 		async function fetchUsers() {
@@ -40,12 +30,13 @@ function Friends() {
 			<FriendsCardsContainer>
 				{users
 					.filter(({ _id }) => _id !== user._id)
-					.filter(({ _id }) => friends?.every((friend) => friend._id !== _id))
-					.map((user) => (
+					.filter(({ _id }) => user.friends.every((friend) => friend._id !== _id))
+					.filter(({ _id }) => user.friendRequests.every((friend) => friend._id !== _id))
+					.map((cardUser) => (
 						<Card
-							key={`card${user._id}`}
-							user={user}
-							handleAddfriends={handleAddfriends}
+							key={`card${cardUser._id}`}
+							currentuser={user}
+							user={cardUser}
 							setUsers={setUsers}
 						/>
 					))}
