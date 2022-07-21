48651afe254e7f51739d73a998ae657d06779b06
Karim <k.guezlane@gmail.com>
2022-07-21 14:20:35 +0100

    reset loaded afetr changing profile user

--- frontend/src/components/profile components/subcomponents/CoverPic.jsx
+++ frontend/src/components/profile components/subcomponents/CoverPic.jsx
@@ -1,12 +1,17 @@
 import { Skeleton } from "@mantine/core";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { handleCPModal } from "../../../features/Modal/modalSlice";
+import { useParams } from "react-router-dom";
 
 function CoverPic({ currentUser, user }) {
 	const dispatch = useDispatch();
 	const [loaded, setloaded] = useState(false);
+	const { id } = useParams();
 
+	useEffect(() => {
+		setloaded(false);
+	}, [id]);
 	return (
 		<Skeleton visible={!loaded} className='wrapper'>
 			<img onLoad={() => setloaded(true)} src={user?.coverPic.url} alt='Cover' />
