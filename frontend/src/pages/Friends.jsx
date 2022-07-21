a905800dbd7579b7b47b3528c8e10f0a097db3db
Karim <k.guezlane@gmail.com>
2022-07-21 18:25:01 +0100

    conditionaly showing sugestion or requests page

--- frontend/src/pages/Friends.jsx
+++ frontend/src/pages/Friends.jsx
@@ -1,12 +1,14 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import { Card } from "../components/friends components";
+import { Card, RequestCard } from "../components/friends components";
 import { Container, FriendsHeader, FriendsCardsContainer } from "../styles/friends";
 import { useDispatch, useSelector } from "react-redux";
 import { getUsers } from "../features/auth/userSlice";
 
 function Friends() {
 	const [users, setUsers] = useState([]);
+	const [page, setPage] = useState("suggestions");
+
 	const { user } = useSelector((state) => state.user);
 	const dispatch = useDispatch();
 
@@ -25,22 +27,52 @@ function Friends() {
 	return (
 		<Container>
 			<FriendsHeader>
-				<h1>People you may know</h1>
+				<button
+					onClick={() =>
+						setPage((page) =>
+							page === "suggestions" ? "Friend Requests" : "suggestions",
+						)
+					}
+				>
+					{page === "suggestions" ? "Friend Requests" : "Suggestions"}
+				</button>
 			</FriendsHeader>
-			<FriendsCardsContainer>
-				{users
-					.filter(({ _id }) => _id !== user._id)
-					.filter(({ _id }) => user.friends.every((friend) => friend._id !== _id))
-					.filter(({ _id }) => user.friendRequests.every((friend) => friend._id !== _id))
-					.map((cardUser) => (
-						<Card
-							key={`card${cardUser._id}`}
-							currentuser={user}
-							user={cardUser}
-							setUsers={setUsers}
-						/>
-					))}
-			</FriendsCardsContainer>
+			{page === "suggestions" && (
+				<>
+					<FriendsHeader>
+						<h1>People you may know</h1>
+					</FriendsHeader>
+					<FriendsCardsContainer>
+						{users
+							.filter(({ _id }) => _id !== user._id)
+							.filter(({ _id }) => user.friends.every((friend) => friend._id !== _id))
+							.filter(({ _id }) =>
+								user.friendRequests.every((friend) => friend._id !== _id),
+							)
+							.map((cardUser) => (
+								<Card
+									key={`card${cardUser._id}`}
+									currentuser={user}
+									user={cardUser}
+									setUsers={setUsers}
+								/>
+							))}
+					</FriendsCardsContainer>
+				</>
+			)}
+			{page !== "suggestions" && (
+				<>
+					<FriendsCardsContainer>
+						{user.friendRequests.map((cardUser) => (
+							<RequestCard
+								key={`card${cardUser._id}`}
+								currentuser={user}
+								user={cardUser}
+							/>
+						))}
+					</FriendsCardsContainer>
+				</>
+			)}
 		</Container>
 	);
 }
