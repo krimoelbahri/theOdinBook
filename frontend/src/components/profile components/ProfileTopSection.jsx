fb444603a5538b6d69ea5bda1f54b4f127082d90
Karim <k.guezlane@gmail.com>
2022-07-21 14:20:01 +0100

    scroll to view when loading profile

--- frontend/src/components/profile components/ProfileTopSection.jsx
+++ frontend/src/components/profile components/ProfileTopSection.jsx
@@ -6,14 +6,21 @@ import {
 } from "../../styles/profile";
 import CoverPic from "./subcomponents/CoverPic";
 import ProfileInfo from "./subcomponents/ProfileInfo";
+import { useScrollIntoView } from "@mantine/hooks";
+import { useEffect } from "react";
 
 function ProfileTopSection({ currentUser, element, user }) {
+	const { scrollIntoView, targetRef } = useScrollIntoView();
+	useEffect(() => {
+		scrollIntoView({ alignment: "center" });
+	}, [scrollIntoView]);
+
 	return (
 		<TopSectionContainer ref={element}>
 			<CoverPicWrapper>
 				<CoverPic currentUser={currentUser} user={user} />
 			</CoverPicWrapper>
-			<ProfileInfoWrapper>
+			<ProfileInfoWrapper ref={targetRef}>
 				<ProfileInfo currentUser={currentUser} user={user} />
 			</ProfileInfoWrapper>
 			<LineWrapper>
