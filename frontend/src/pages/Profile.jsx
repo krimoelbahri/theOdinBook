80c3f84308299989e30ddaaf636330900b160609
Karim <k.guezlane@gmail.com>
2022-07-22 22:16:07 +0100

    fetching profile user

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,36 +1,26 @@
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { useLocation, useParams } from "react-router-dom";
 import {
 	ProfileTopSection,
 	ProfileNavSection,
 	ProfileBottomSection,
 } from "../components/profile components";
-import { getUser, reset } from "../features/auth/userSlice";
 import { useElementOnScreen } from "../hooks/intersectionObserver";
-import { useGetProfilePostsQuery } from "../features/posts/post-api-query";
+import { useGetUserQuery } from "../features/auth/user-api-query";
 
 function Profile() {
 	const [ref, isVisible] = useElementOnScreen({ rootMargin: "-60px" });
 	const { id } = useParams();
 
-	const dispatch = useDispatch();
-	const { user, profileUser } = useSelector((state) => state.user);
-	const { data } = useGetProfilePostsQuery(id);
-
+	const { user } = useSelector((state) => state.user);
+	const { data = [] } = useGetUserQuery(id);
 	const location = useLocation();
-
-	useEffect(() => {
-		dispatch(getUser(id));
-
-		return () => dispatch(reset());
-	}, [id, user, dispatch]);
 
 	return (
 		<>
-			<ProfileTopSection currentUser={id === user._id} element={ref} user={profileUser} />
-			<ProfileNavSection visible={isVisible} location={location} user={profileUser} />
-			<ProfileBottomSection currentUser={id === user._id} post={data} user={profileUser} />
+			<ProfileTopSection currentUser={id === user._id} element={ref} user={data} />
+			<ProfileNavSection visible={isVisible} location={location} user={data} />
+			<ProfileBottomSection currentUser={id === user._id} id={id} user={data} />
 		</>
 	);
 }
