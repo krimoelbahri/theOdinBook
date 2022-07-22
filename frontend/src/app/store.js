99bde62df164c8cd45621df67b90ec3808dc45af
Karim <k.guezlane@gmail.com>
2022-07-22 20:48:11 +0100

    added postApi reducer to store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -3,11 +3,17 @@ import userReducer from "../features/auth/userSlice";
 import dropDownReducer from "../features/dropDown/dropDownSlice";
 import modalReducer from "../features/Modal/modalSlice";
 import postReducer from "../features/posts/postSlice";
+import { postApi } from "../features/posts/post-api-query";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
 export const store = configureStore({
 	reducer: {
 		user: userReducer,
 		post: postReducer,
 		dropDown: dropDownReducer,
 		modal: modalReducer,
+		[postApi.reducerPath]: postApi.reducer,
 	},
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(postApi.middleware),
 });
+
+setupListeners(store.dispatch);
