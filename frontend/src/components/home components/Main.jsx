41817790e1bc853c9ea45a143f803d33eb7bc509
Karim <k.guezlane@gmail.com>
2022-07-22 20:48:26 +0100

    maping through posts

--- frontend/src/components/home components/Main.jsx
+++ frontend/src/components/home components/Main.jsx
@@ -2,10 +2,11 @@ import { MainContainer } from "../../styles/home.styled";
 import { Post, CreatePost } from "../post components";
 
 function Main({ post }) {
+	console.log(post);
 	return (
 		<MainContainer>
 			<CreatePost />
-			{post.posts?.map((post, i) => (
+			{post?.map((post, i) => (
 				<Post key={post._id} post={post} postIndex={i} />
 			))}
 		</MainContainer>
