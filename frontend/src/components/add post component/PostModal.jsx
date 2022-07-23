1dba0e10f13c59f7c4a9fdbc13f826ae29d7e0b9
Karim <k.guezlane@gmail.com>
2022-07-23 16:37:29 +0100

    Adding post using RTK query

--- frontend/src/components/add post component/PostModal.jsx
+++ frontend/src/components/add post component/PostModal.jsx
@@ -1,30 +1,38 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addingPost, addPosts, resetAddPost } from "../../features/posts/postSlice";
 import { ModalContainer, Loader } from "../../styles/Modals";
 import { handlePostModal } from "../../features/Modal/modalSlice";
 import ModalbottomSection from "./Modal subcomponents/ModalbottomSection";
 import ModalMediaSection from "./Modal subcomponents/ModalMediaSection";
 import ModalTopSection from "./Modal subcomponents/ModalTopSection";
+import { useAddPostMutation } from "../../features/posts/post-api-query";
 
 function PostModal() {
 	//using Redux
 	const dispatch = useDispatch();
 	const { user } = useSelector((state) => state.user);
-	const { addPost } = useSelector((state) => state.post);
+	const [addPost] = useAddPostMutation();
+
 	//useState state handeling post data
 	const [media, setMedia] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
 	const [url, setUrl] = useState(null);
-	const [data, setData] = useState({ description: null, author: user._id, imgFile: null });
+	const [_formData, setData] = useState({ description: null, author: user._id, imgFile: null });
 
 	async function handleSubmit(e) {
 		e.preventDefault();
+		let formData = new FormData();
+		formData.append("imgFile", _formData.imgFile);
+		formData.append("description", _formData.description);
+		formData.append("author", _formData.author);
+
 		try {
-			let post = await dispatch(addingPost(data)).unwrap();
-			dispatch(addPosts(post));
+			setIsLoading(true);
+			await addPost(formData);
+			setIsLoading(false);
 			dispatch(handlePostModal(false));
-			dispatch(resetAddPost());
 		} catch (error) {
+			setIsLoading(false);
 			console.log(error);
 		}
 	}
@@ -40,7 +48,7 @@ function PostModal() {
 				setData={setData}
 			/>
 			<ModalbottomSection setMedia={setMedia} setUrl={setUrl} />
-			{addPost.isLoading && (
+			{isLoading && (
 				<Loader>
 					<i className='fa-solid fa-spinner fa-spin-pulse'></i>
 				</Loader>
