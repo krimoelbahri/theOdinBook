e9f54c9eb336843585bfbcd29feba15246dd44ad
Karim <k.guezlane@gmail.com>
2022-07-23 16:40:51 +0100

    cleaning

--- frontend/src/features/posts/postSlice.js
+++ frontend/src/features/posts/postSlice.js
@@ -1,21 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import postServices from "./postServices";
-let state = {
-	post: { posts: [], isError: false, isDone: false, isLoading: false, message: "" },
-};
+let state = {};
 
-export const addingPost = createAsyncThunk("add/Post", async (data, thunkAPI) => {
-	const { description, author, imgFile } = data;
-	let formData = new FormData();
-	formData.append("imgFile", imgFile);
-	try {
-		let data = await postServices.uploadImage(formData);
-		let response = await postServices.addPost({ description, author, postImage: data });
-		return response;
-	} catch (error) {
-		return thunkAPI.rejectWithValue(error.response.data.message);
-	}
-});
 export const addingComment = createAsyncThunk("add/comment", async (data, thunkAPI) => {
 	const { text, author, postId } = data;
 	try {
@@ -43,26 +29,11 @@ export const addingLike = createAsyncThunk("add/comment", async (data, thunkAPI)
 		return thunkAPI.rejectWithValue(error.response.data.message);
 	}
 });
-export const deletePost = createAsyncThunk("add/comment", async (postId, thunkAPI) => {
-	try {
-		let response = await postServices.deletePost(postId);
-		return response;
-	} catch (error) {
-		return thunkAPI.rejectWithValue(error.response.data.message);
-	}
-});
 
 let postSlice = createSlice({
 	name: "post",
 	initialState: state,
-	reducers: {
-		addPosts: ({ post }, action) => {
-			post.posts.unshift(action.payload);
-		},
-		removePost: ({ post }, action) => {
-			post.posts.splice(action.payload, 1);
-		},
-	},
+	reducers: {},
 });
 export const { resetPost, resetAddPost, removePost, addPosts } = postSlice.actions;
 export default postSlice.reducer;
