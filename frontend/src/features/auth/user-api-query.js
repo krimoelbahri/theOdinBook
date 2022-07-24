bf30ac50fb6e3229bde7ae9c814882df97ef46be
Karim <k.guezlane@gmail.com>
2022-07-24 18:55:23 +0100

    added update user mutation

--- frontend/src/features/auth/user-api-query.js
+++ frontend/src/features/auth/user-api-query.js
@@ -1,5 +1,14 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
 let URL = "/api/user/";
+
+function getConfig() {
+	let user = JSON.parse(localStorage.getItem("user"));
+	const config = {
+		Authorization: `Bearer ${user?.token}`,
+	};
+	return config;
+}
 
 const baseQuery = fetchBaseQuery({
 	baseUrl: URL,
@@ -8,14 +17,31 @@ const baseQuery = fetchBaseQuery({
 export const userApi = createApi({
 	reducerPath: "userApi",
 	baseQuery,
+	tagTypes: ["User"],
 	endpoints: (builder) => ({
 		getUsers: builder.query({
 			query: () => ({ url: `getUsers` }),
+			providesTags: ["User"],
 		}),
 		getUser: builder.query({
-			query: (id) => ({ url: id }),
+			query: (id) => ({ url: `${id}` }),
+			providesTags: ["User"],
+		}),
+		updateUser: builder.mutation({
+			query: ({ id, ...rest }) => {
+				let formData = new FormData();
+				formData.append("imgFile", rest.imgFile);
+				formData.append("action", rest.action);
+				return {
+					url: `${id}`,
+					method: "PUT",
+					body: formData,
+					headers: getConfig(),
+				};
+			},
+			invalidatesTags: ["User"],
 		}),
 	}),
 });
 
-export const { useGetUserQuery, useGetUsersQuery } = userApi;
+export const { useGetUserQuery, useGetUsersQuery, useUpdateUserMutation } = userApi;
