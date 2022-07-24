45f37b426bc4ffd7d26e10608d26743ee2382ea3
Karim <k.guezlane@gmail.com>
2022-07-24 18:52:27 +0100

    Mantine notification provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,7 @@ import "./index.css";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
+import { NotificationsProvider } from "@mantine/notifications";
 
 const container = document.getElementById("app");
 const root = createRoot(container);
@@ -15,9 +16,18 @@ root.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<ThemeProvider theme={theme}>
-				<BrowserRouter>
-					<App />
-				</BrowserRouter>
+				<NotificationsProvider
+					autoClose={2000}
+					limit={3}
+					notificationMaxHeight={120}
+					containerWidth={300}
+					zIndex={2000}
+					position={"bottom-left"}
+				>
+					<BrowserRouter>
+						<App />
+					</BrowserRouter>
+				</NotificationsProvider>
 			</ThemeProvider>
 		</Provider>
 	</React.StrictMode>,
