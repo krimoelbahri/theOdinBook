f04993cf4e0edd1f752514f3e2cf8c2ab49fcdb6
Karim <k.guezlane@gmail.com>
2022-07-24 18:56:44 +0100

    locking screen when showing modal

--- frontend/src/pages/Modals.jsx
+++ frontend/src/pages/Modals.jsx
@@ -2,10 +2,22 @@ import { Container } from "../styles/Modals";
 import PostModal from "../components/add post component";
 import { useSelector } from "react-redux";
 import { CoverPicModal, ProfilePicModal } from "../components/profile components";
+import { useScrollLock } from "@mantine/hooks";
+import { useEffect } from "react";
 
 function Modals() {
 	const { postModal, coverPicModal, profilePicModal } = useSelector((state) => state.modal);
 	const { user } = useSelector((state) => state.user);
+	// eslint-disable-next-line no-unused-vars
+	const [scrollLocked, setScrollLocked] = useScrollLock();
+
+	useEffect(() => {
+		if (postModal || coverPicModal || profilePicModal) {
+			setScrollLocked(true);
+		} else {
+			setScrollLocked(false);
+		}
+	}, [postModal, coverPicModal, profilePicModal, setScrollLocked]);
 
 	return (
 		<>
