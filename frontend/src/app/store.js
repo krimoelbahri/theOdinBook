ac637a6235c880af6c27f82313cce9de0426cbb5
Karim <k.guezlane@gmail.com>
2022-07-24 22:31:48 +0100

    cleaning

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -2,7 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/auth/userSlice";
 import dropDownReducer from "../features/dropDown/dropDownSlice";
 import modalReducer from "../features/Modal/modalSlice";
-import postReducer from "../features/posts/postSlice";
 import { postApi } from "../features/posts/post-api-query";
 import { userApi } from "../features/auth/user-api-query";
 
@@ -10,7 +9,6 @@ import { setupListeners } from "@reduxjs/toolkit/dist/query";
 export const store = configureStore({
 	reducer: {
 		user: userReducer,
-		post: postReducer,
 		dropDown: dropDownReducer,
 		modal: modalReducer,
 		[postApi.reducerPath]: postApi.reducer,
