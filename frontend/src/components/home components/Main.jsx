06def5320990ff88d9d5049188d32e54ad04502d
Karim <k.guezlane@gmail.com>
2022-07-25 18:23:10 +0100

    showing spinner when loading posts

--- frontend/src/components/home components/Main.jsx
+++ frontend/src/components/home components/Main.jsx
@@ -1,13 +1,27 @@
 import { MainContainer } from "../../styles/home.styled";
 import { Post, CreatePost } from "../post components";
+import Spinner from "../Spinner";
+import { useGetUserPostsQuery } from "../../features/posts/post-api-query";
+import { useEffect } from "react";
+import { errorNotification } from "../../helpers/notification";
 
-function Main({ post }) {
+function Main() {
+	const { data = [], isLoading, isError, error } = useGetUserPostsQuery();
+	useEffect(() => {
+		if (isError) errorNotification(error, "m-p-Error");
+	}, [isError, error]);
 	return (
 		<MainContainer>
 			<CreatePost />
-			{post?.map((post, i) => (
-				<Post key={post._id} post={post} />
-			))}
+			{isLoading ? (
+				<Spinner />
+			) : (
+				<>
+					{data?.map((post) => (
+						<Post key={post._id} post={post} />
+					))}
+				</>
+			)}
 		</MainContainer>
 	);
 }
