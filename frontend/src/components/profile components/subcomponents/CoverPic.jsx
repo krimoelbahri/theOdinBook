29ee852f5775031f437dad3299c9c0026b43fdd2
Karim <k.guezlane@gmail.com>
2022-07-25 18:25:31 +0100

    handeling skeletons in profile

--- frontend/src/components/profile components/subcomponents/CoverPic.jsx
+++ frontend/src/components/profile components/subcomponents/CoverPic.jsx
@@ -3,12 +3,12 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { handleCPModal } from "../../../features/Modal/modalSlice";
 
-function CoverPic({ currentUser, user }) {
+function CoverPic({ currentUser, user, loading }) {
 	const dispatch = useDispatch();
 	const [loaded, setloaded] = useState(false);
 
 	return (
-		<Skeleton visible={!loaded} className='wrapper'>
+		<Skeleton visible={loading && !loaded} className='wrapper'>
 			<img onLoad={() => setloaded(true)} src={user?.coverPic?.url} alt='Cover' />
 			{currentUser && (
 				<button onClick={() => dispatch(handleCPModal(true))}>
