6ab17bda7a04f6d5fc23d9b5b0387cce1da02440
Karim <k.guezlane@gmail.com>
2022-07-25 18:28:38 +0100

    cleaning

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,13 @@
 import { useSelector } from "react-redux";
 import { LeftBar, RightBar, Main } from "../components/home components";
 import { Container } from "../styles/home.styled";
-import { useGetUserPostsQuery } from "../features/posts/post-api-query";
 const Home = () => {
 	const { user } = useSelector((state) => state.user);
-	const { data = [] } = useGetUserPostsQuery();
 
 	return (
 		<Container>
 			<LeftBar />
-			<Main post={data} />
+			<Main />
 			<RightBar friends={user?.friends} />
 		</Container>
 	);
