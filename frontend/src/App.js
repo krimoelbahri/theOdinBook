36789578da559f5bea93d7bbc9b58b81ba905540
Karim <k.guezlane@gmail.com>
2022-07-26 16:29:23 +0100

    providing user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import Modals from "./pages/Modals";
 import GlobalFonts from "./fonts/fonts";
 import MyRoutes from "./MyRoutes";
+import { useGetCurrentUserQuery } from "./features/auth/user-api-query";
+
+const AuthContext = React.createContext();
+export function useAuth() {
+	return useContext(AuthContext);
+}
 
 function App() {
+	const { data: user } = useGetCurrentUserQuery();
 	return (
-		<>
+		<AuthContext.Provider value={{ user }}>
 			<MyRoutes />
 			<Modals />
 			<GlobalFonts />
-		</>
+		</AuthContext.Provider>
 	);
 }
 
