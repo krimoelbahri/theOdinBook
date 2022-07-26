8b71095f74014b12ac78e6288a2fcee381cc6ab6
Karim <k.guezlane@gmail.com>
2022-07-26 16:33:13 +0100

    cleaning

--- frontend/src/features/auth/userServices.js
+++ frontend/src/features/auth/userServices.js
@@ -22,20 +22,7 @@ const friendRequestreply = async function ({ author, friend, action }) {
 	);
 	return response.data;
 };
-const signup = async function (userData) {
-	let response = await axios.post(URL + "signup", userData);
-	if (response.data) {
-		localStorage.setItem("user", JSON.stringify(response.data));
-	}
-	return response.data;
-};
-const signin = async function (userData) {
-	let response = await axios.post(URL + "auth/local", userData);
-	if (response.data) {
-		localStorage.setItem("user", JSON.stringify(response.data));
-	}
-	return response.data;
-};
+
 const facebookSignin = async function () {
 	let response = await axios.get(URL + "auth/facebook");
 	console.log(response);
@@ -44,19 +31,9 @@ const facebookSignin = async function () {
 	}
 	return response.data;
 };
-const logout = async () => {
-	let response = await axios.get(URL + "auth/logout");
-	if (response.data) {
-		localStorage.removeItem("user");
-	}
-	return response.data;
-};
 
 let userServices = {
-	signup,
-	signin,
 	facebookSignin,
-	logout,
 	friendRequest,
 	friendRequestreply,
 };
