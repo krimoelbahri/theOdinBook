577bd907acf6aaa9a4390debfa80ab291a9e865b
Karim <k.guezlane@gmail.com>
2022-07-26 14:14:52 +0100

    handeling loading state

--- frontend/src/components/profile components/ProfileBottomSection.jsx
+++ frontend/src/components/profile components/ProfileBottomSection.jsx
@@ -11,8 +11,8 @@ import { useGetProfilePostsQuery } from "../../features/posts/post-api-query";
 import { errorNotification } from "../../helpers/notification";
 import Spinner from "../Spinner";
 
-function ProfileBottomSection({ currentUser, id, user }) {
-	const { data = [], isError, error, isFetching } = useGetProfilePostsQuery(id);
+function ProfileBottomSection({ currentUser, id, user, loading }) {
+	const { currentData, isError, error, isFetching } = useGetProfilePostsQuery(id);
 	useEffect(() => {
 		if (isError) errorNotification(error, "p-p-Error");
 	}, [isError, error]);
@@ -31,26 +31,32 @@ function ProfileBottomSection({ currentUser, id, user }) {
 							</Link>
 						</div>
 						<div className='wrapper'>
-							{user?.friends?.map((friend, i) => (
-								<Link
-									to={`/${friend._id}`}
-									key={`card${friend._id}`}
-									className='card'
-								>
-									<img src={friend?.profilePic?.url} alt='card' />
-									<h3>{friend.name}</h3>
-								</Link>
-							))}
+							{loading ? (
+								<Spinner pos={"relative"} />
+							) : (
+								<>
+									{user?.friends?.map((friend, i) => (
+										<Link
+											to={`/${friend._id}`}
+											key={`card${friend._id}`}
+											className='card'
+										>
+											<img src={friend?.profilePic?.url} alt='card' />
+											<h3>{friend.name}</h3>
+										</Link>
+									))}
+								</>
+							)}
 						</div>
 					</FriendsContainer>
 				</ProfileLeftBar>
 				<ProfilePostsWrapper>
 					{currentUser && <CreatePost />}
-					{isFetching ? (
+					{isFetching && !currentData ? (
 						<Spinner />
 					) : (
 						<>
-							{data?.map((post) => (
+							{currentData?.map((post) => (
 								<Post key={post._id} post={post} />
 							))}
 						</>
