1a80a885251efc074d36ab025d5ed8605c5084e8
Karim <k.guezlane@gmail.com>
2023-07-15 13:43:36 +0100

    cleaning

--- app/page.js
+++ app/page.js
@@ -1,9 +1,7 @@
-import Image from 'next/image'
-
 export default function Home() {
-  return (
+	return (
 		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
 			<h>Hello World</h>
 		</main>
-  );
+	);
 }
